c3708846c69cf3518392f7b23ec07e9890734de0
Seth Messer <seth.messer@gmail.com>
2018-06-02 21:31:40 -0500

    minor isntallation updates

--- zsh/themes/all.sh
+++ zsh/themes/all.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/zsh
 
+echo "Symlinking custom zsh theme to /usr/local/share/zsh/site-functions"
+
 ln -sfv $DOTS/zsh/themes/prompt_pure.zsh /usr/local/share/zsh/site-functions/prompt_pure_setup
 ln -sfv $DOTS/zsh/themes/async.zsh /usr/local/share/zsh/site-functions/async
 # ln -sfv $DOTS/zsh/themes/gitstatus.zsh /usr/local/share/zsh/site-functions/gitstatus
