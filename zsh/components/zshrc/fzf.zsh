958247621928d40a73181e754880fa3fef7dc132
Seth Messer <seth.messer@gmail.com>
2018-06-07 10:50:11 -0500

    too many broken things right now with fzf, my snippets, etc)

--- zsh/components/zshrc/fzf.zsh
+++ zsh/components/zshrc/fzf.zsh
@@ -9,206 +9,206 @@ export FZF_COMPLETION_TRIGGER=''
 bindkey '^G' fzf-completion
 bindkey '^I' $fzf_default_completion
 
-# ==/ FZF Helpers /============================================================
-# graciously thieved from: https://github.com/junegunn/fzf/wiki/Examples
+# # ==/ FZF Helpers /============================================================
+# # graciously thieved from: https://github.com/junegunn/fzf/wiki/Examples
 
-prev() {
-  git ls-files | fzf --preview "pygmentize {}" --color light --margin 5,20
-}
-
-# fe [FUZZY PATTERN] - Open the selected file with the default editor
-#   - Bypass fuzzy finder if there's only one match (--select-1)
-#   - Exit if there's no match (--exit-0)
-fe() {
-  IFS='
-'
-  local declare files=($(fzf-tmux --query="$1" --select-1 --exit-0))
-  [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
-  unset IFS
-}
-
-# vf - fuzzy open with vim from anywhere
-# ex: vf word1 word2 ... (even part of a file name)
-# zsh autoload function
-vf() {
-  local files
-
-  files=(${(f)"$(locate -Ai -0 $@ | grep -z -vE '~$' | fzf-tmux --read0 -0 -1 -m)"})
-
-  if [[ -n $files ]]
-  then
-     vim -- $files
-     print -l $files[1]
-  fi
-}
-
-# fd - cd to selected directory
-# ::: commenting this for now because i've got an `fd` binary from homebrew now
-# fd() {
-#   local dir
-#   dir=$(find ${1:-*} -path '*/\.*' -prune \
-#                   -o -type d -print 2> /dev/null | fzf-tmux +m) &&
-#   cd "$dir"
+# prev() {
+#   git ls-files | fzf --preview "pygmentize {}" --color light --margin 5,20
 # }
 
-# fda - including hidden directories
-fda() {
-  local dir
-  dir=$(find ${1:-.} -type d 2> /dev/null | fzf-tmux +m) && cd "$dir"
-}
+# # fe [FUZZY PATTERN] - Open the selected file with the default editor
+# #   - Bypass fuzzy finder if there's only one match (--select-1)
+# #   - Exit if there's no match (--exit-0)
+# fe() {
+#   IFS='
+# '
+#   local declare files=($(fzf-tmux --query="$1" --select-1 --exit-0))
+#   [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
+#   unset IFS
+# }
 
-# cf - fuzzy cd from anywhere
-# ex: cf word1 word2 ... (even part of a file name)
-# zsh autoload function
-cf() {
-  local file
+# # vf - fuzzy open with vim from anywhere
+# # ex: vf word1 word2 ... (even part of a file name)
+# # zsh autoload function
+# vf() {
+#   local files
 
-  file="$(locate -Ai -0 $@ | grep -z -vE '~$' | fzf-tmux --read0 -0 -1)"
+#   files=(${(f)"$(locate -Ai -0 $@ | grep -z -vE '~$' | fzf-tmux --read0 -0 -1 -m)"})
 
-  if [[ -n $file ]]
-  then
-     if [[ -d $file ]]
-     then
-        cd -- $file
-     else
-        cd -- ${file:h}
-     fi
-  fi
-}
+#   if [[ -n $files ]]
+#   then
+#      vim -- $files
+#      print -l $files[1]
+#   fi
+# }
 
-# fkill - kill process
-fkill() {
-  pid=$(ps -ef | sed 1d | fzf-tmux -m | awk '{print $2}')
+# # fd - cd to selected directory
+# # ::: commenting this for now because i've got an `fd` binary from homebrew now
+# # fd() {
+# #   local dir
+# #   dir=$(find ${1:-*} -path '*/\.*' -prune \
+# #                   -o -type d -print 2> /dev/null | fzf-tmux +m) &&
+# #   cd "$dir"
+# # }
 
-  if [ "x$pid" != "x" ]
-  then
-    kill -${1:-9} $pid
-  fi
-}
+# # fda - including hidden directories
+# fda() {
+#   local dir
+#   dir=$(find ${1:-.} -type d 2> /dev/null | fzf-tmux +m) && cd "$dir"
+# }
 
-# - GIT ------------------
-# fbr - checkout git branch
-fbr() {
-  local branches branch
-  branches=$(git branch -vv) &&
-  branch=$(echo "$branches" | fzf-tmux +m) &&
-  git checkout $(echo "$branch" | awk '{print $1}' | sed "s/.* //")
-}
+# # cf - fuzzy cd from anywhere
+# # ex: cf word1 word2 ... (even part of a file name)
+# # zsh autoload function
+# cf() {
+#   local file
 
-# fbr - checkout git branch (including remote branches)
-fbr() {
-  local branches branch
-  branches=$(git branch --all | grep -v HEAD) &&
-  branch=$(echo "$branches" |
-           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
-  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
-}
+#   file="$(locate -Ai -0 $@ | grep -z -vE '~$' | fzf-tmux --read0 -0 -1)"
 
-# fco - checkout git branch/tag
-fco() {
-  local tags branches target
-  tags=$(
-    git tag | awk '{print "\x1b[31;1mtag\x1b[m\t" $1}') || return
-  branches=$(
-    git branch --all | grep -v HEAD             |
-    sed "s/.* //"    | sed "s#remotes/[^/]*/##" |
-    sort -u          | awk '{print "\x1b[34;1mbranch\x1b[m\t" $1}') || return
-  target=$(
-    (echo "$tags"; echo "$branches") |
-    fzf-tmux -l30 -- --no-hscroll --ansi +m -d "\t" -n 2) || return
-  git checkout $(echo "$target" | awk '{print $2}')
-}
+#   if [[ -n $file ]]
+#   then
+#      if [[ -d $file ]]
+#      then
+#         cd -- $file
+#      else
+#         cd -- ${file:h}
+#      fi
+#   fi
+# }
 
-# fcoc - checkout git commit
-fcoc() {
-  local commits commit
-  commits=$(git log --pretty=oneline --abbrev-commit --reverse) &&
-  commit=$(echo "$commits" | fzf-tmux --tac +s +m -e) &&
-  git checkout $(echo "$commit" | sed "s/ .*//")
-}
+# # fkill - kill process
+# fkill() {
+#   pid=$(ps -ef | sed 1d | fzf-tmux -m | awk '{print $2}')
 
-# fshow - git commit browser
-fshow() {
-  git log --graph --color=always \
-      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
-  fzf-tmux --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
-      --bind "ctrl-m:execute:
-                (grep -o '[a-f0-9]\{7\}' | head -1 |
-                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
-                {}
-FZF-EOF"
-}
+#   if [ "x$pid" != "x" ]
+#   then
+#     kill -${1:-9} $pid
+#   fi
+# }
 
-# fcs - get git commit sha
-# example usage: git rebase -i `fcs`
-fcs() {
-  local commits commit
-  commits=$(git log --color=always --pretty=oneline --abbrev-commit --reverse) &&
-  commit=$(echo "$commits" | fzf-tmux --tac +s +m -e --ansi --reverse) &&
-  echo -n $(echo "$commit" | sed "s/ .*//")
-}
+# # - GIT ------------------
+# # fbr - checkout git branch
+# fbr() {
+#   local branches branch
+#   branches=$(git branch -vv) &&
+#   branch=$(echo "$branches" | fzf-tmux +m) &&
+#   git checkout $(echo "$branch" | awk '{print $1}' | sed "s/.* //")
+# }
 
-# fstash - easier way to deal with stashes
-# type fstash to get a list of your stashes
-# enter shows you the contents of the stash
-# ctrl-d shows a diff of the stash against your current HEAD
-# ctrl-b checks the stash out as a branch, for easier merging
-fstash() {
-  local out q k sha
-    while out=$(
-      git stash list --pretty="%C(yellow)%h %>(14)%Cgreen%cr %C(blue)%gs" |
-      fzf-tmux --ansi --no-sort --query="$q" --print-query \
-          --expect=ctrl-d,ctrl-b);
-    do
-      q=$(head -1 <<< "$out")
-      k=$(head -2 <<< "$out" | tail -1)
-      sha=$(tail -1 <<< "$out" | cut -d' ' -f1)
-      [ -z "$sha" ] && continue
-      if [ "$k" = 'ctrl-d' ]; then
-        git diff $sha
-      elif [ "$k" = 'ctrl-b' ]; then
-        git stash branch "stash-$sha" $sha
-        break;
-      else
-        git stash show -p $sha
-      fi
-    done
-}
+# # fbr - checkout git branch (including remote branches)
+# fbr() {
+#   local branches branch
+#   branches=$(git branch --all | grep -v HEAD) &&
+#   branch=$(echo "$branches" |
+#            fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+#   git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+# }
 
-# Use `fzf` to browse/select a recent git branch
-# h/t @jeremywrowe
-cb() {
-  git checkout $(git short-recent | fzf)
-}
+# # fco - checkout git branch/tag
+# fco() {
+#   local tags branches target
+#   tags=$(
+#     git tag | awk '{print "\x1b[31;1mtag\x1b[m\t" $1}') || return
+#   branches=$(
+#     git branch --all | grep -v HEAD             |
+#     sed "s/.* //"    | sed "s#remotes/[^/]*/##" |
+#     sort -u          | awk '{print "\x1b[34;1mbranch\x1b[m\t" $1}') || return
+#   target=$(
+#     (echo "$tags"; echo "$branches") |
+#     fzf-tmux -l30 -- --no-hscroll --ansi +m -d "\t" -n 2) || return
+#   git checkout $(echo "$target" | awk '{print $2}')
+# }
 
-# - GIT ------------------
-# fs [FUZZY PATTERN] - Select selected tmux session
-#   - Bypass fuzzy finder if there's only one match (--select-1)
-#   - Exit if there's no match (--exit-0)
-fs() {
-  local session
-  session=$(tmux list-sessions -F "#{session_name}" | \
-    fzf-tmux --query="$1" --select-1 --exit-0) &&
-  tmux switch-client -t "$session"
-}
+# # fcoc - checkout git commit
+# fcoc() {
+#   local commits commit
+#   commits=$(git log --pretty=oneline --abbrev-commit --reverse) &&
+#   commit=$(echo "$commits" | fzf-tmux --tac +s +m -e) &&
+#   git checkout $(echo "$commit" | sed "s/ .*//")
+# }
 
-# - Z ------------------
+# # fshow - git commit browser
+# fshow() {
+#   git log --graph --color=always \
+#       --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+#   fzf-tmux --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
+#       --bind "ctrl-m:execute:
+#                 (grep -o '[a-f0-9]\{7\}' | head -1 |
+#                 xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+#                 {}
+# FZF-EOF"
+# }
+
+# # fcs - get git commit sha
+# # example usage: git rebase -i `fcs`
+# fcs() {
+#   local commits commit
+#   commits=$(git log --color=always --pretty=oneline --abbrev-commit --reverse) &&
+#   commit=$(echo "$commits" | fzf-tmux --tac +s +m -e --ansi --reverse) &&
+#   echo -n $(echo "$commit" | sed "s/ .*//")
+# }
+
+# # fstash - easier way to deal with stashes
+# # type fstash to get a list of your stashes
+# # enter shows you the contents of the stash
+# # ctrl-d shows a diff of the stash against your current HEAD
+# # ctrl-b checks the stash out as a branch, for easier merging
+# fstash() {
+#   local out q k sha
+#     while out=$(
+#       git stash list --pretty="%C(yellow)%h %>(14)%Cgreen%cr %C(blue)%gs" |
+#       fzf-tmux --ansi --no-sort --query="$q" --print-query \
+#           --expect=ctrl-d,ctrl-b);
+#     do
+#       q=$(head -1 <<< "$out")
+#       k=$(head -2 <<< "$out" | tail -1)
+#       sha=$(tail -1 <<< "$out" | cut -d' ' -f1)
+#       [ -z "$sha" ] && continue
+#       if [ "$k" = 'ctrl-d' ]; then
+#         git diff $sha
+#       elif [ "$k" = 'ctrl-b' ]; then
+#         git stash branch "stash-$sha" $sha
+#         break;
+#       else
+#         git stash show -p $sha
+#       fi
+#     done
+# }
+
+# # Use `fzf` to browse/select a recent git branch
+# # h/t @jeremywrowe
+# cb() {
+#   git checkout $(git short-recent | fzf)
+# }
+
+# # - GIT ------------------
+# # fs [FUZZY PATTERN] - Select selected tmux session
+# #   - Bypass fuzzy finder if there's only one match (--select-1)
+# #   - Exit if there's no match (--exit-0)
+# fs() {
+#   local session
+#   session=$(tmux list-sessions -F "#{session_name}" | \
+#     fzf-tmux --query="$1" --select-1 --exit-0) &&
+#   tmux switch-client -t "$session"
+# }
+
+# # - Z ------------------
+# # unalias z 2> /dev/null
+# # z() {
+# #   [ $# -gt 0 ] && _z "$*" && return
+# #   cd "$(_z -l 2>&1 | fzf-tmux +s --tac --query "$*" | sed 's/^[0-9,.]* *//')"
+# # }
+
 # unalias z 2> /dev/null
 # z() {
-#   [ $# -gt 0 ] && _z "$*" && return
-#   cd "$(_z -l 2>&1 | fzf-tmux +s --tac --query "$*" | sed 's/^[0-9,.]* *//')"
+#   if [[ -z "$*" ]]; then
+#     cd "$(_z -l 2>&1 | fzf-tmux +s --tac | sed 's/^[0-9,.]* *//')"
+#   else
+#     _last_z_args="$@"
+#     _z "$@"
+#   fi
 # }
 
-unalias z 2> /dev/null
-z() {
-  if [[ -z "$*" ]]; then
-    cd "$(_z -l 2>&1 | fzf-tmux +s --tac | sed 's/^[0-9,.]* *//')"
-  else
-    _last_z_args="$@"
-    _z "$@"
-  fi
-}
-
-# zz() {
-#   cd "$(_z -l 2>&1 | sed 's/^[0-9,.]* *//' | fzf-tmux -q $_last_z_args)"
-# }
+# # zz() {
+# #   cd "$(_z -l 2>&1 | sed 's/^[0-9,.]* *//' | fzf-tmux -q $_last_z_args)"
+# # }
