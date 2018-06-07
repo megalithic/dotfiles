01f68070ce8ea37a0a3d3454cd09d1dc01bf9dd0
Seth Messer <seth.messer@gmail.com>
2018-06-07 11:00:43 -0500

    fix zsh-syntax-highlighting

--- zsh/components/zshrc/syntax-highlighting.zsh
+++ zsh/components/zshrc/syntax-highlighting.zsh
@@ -1,6 +1,4 @@
 #!/bin/zsh
 
 # zsh-syntax-highlighting init
-if [[ $ZSH_EVAL_CONTEXT == 'file' ]]; then
-  source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-fi
+source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
