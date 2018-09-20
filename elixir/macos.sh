c7cdbe7746398d7c871e4a35817af0e3dcca883f
Seth Messer <seth.messer@gmail.com>
2018-09-19 20:53:32 -0500

    automate elixir-ls installation so we're not stuck with 0.2.23

--- elixir/macos.sh
+++ elixir/macos.sh
@@ -1,4 +1,14 @@
 #!/bin/zsh
 
-mix local.hex
+if [[ ! -d $HOME/.elixir-ls ]]
+then
+  mkdir $HOME/.elixir-ls
+fi
+
+cd $HOME/.elixir-ls
+wget https://github.com/$(wget https://github.com/jakebecker/elixir-ls/releases/latest -O - | egrep '/.*/.*/.*zip' -o)
+unzip elixir-ls.zip
+rm elixir-ls.zip
+
+mix local.hex --force --if-missing
 
