7e71c50d77413f791d1dcef7630f2d5dc1a042fd
Seth Messer <seth.messer@gmail.com>
2018-10-08 11:12:25 -0500

    adds flag for selected window in tmux (thanks @evantravers !!)

--- zsh/components/zshrc/opts.zsh
+++ zsh/components/zshrc/opts.zsh
@@ -15,8 +15,8 @@ umask 022
 # autoload -U $ZSH/functions/*(:t)
 
 HISTFILE=~/.zsh_history
-HISTSIZE=10000
-SAVEHIST=10000
+HISTSIZE=1000
+SAVEHIST=1000
 
 # for eager loading all files for ternjs
 #  - https://github.com/carlitux/deoplete-ternjs
