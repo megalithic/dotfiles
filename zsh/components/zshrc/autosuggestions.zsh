c1c81d08dedb171fc4cf87096ea5a0e40c3bb7b2
Seth Messer <seth.messer@gmail.com>
2018-10-09 17:46:00 -0500

    sets some defaults for zsh-autosuggestions

--- zsh/components/zshrc/autosuggestions.zsh
+++ zsh/components/zshrc/autosuggestions.zsh
@@ -2,3 +2,9 @@
 
 # zsh-autosuggestions init
 source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+# zsh-autosuggestions config;
+# more options here: https://github.com/alyptik/dotfiles/blob/master/.zsh.d/.zshrc#L236
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=244'
+ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
+ZSH_AUTOSUGGEST_USE_ASYNC=1
