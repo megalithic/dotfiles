6d83739beb81ccea1430d5a902229e476ee2f060
Seth Messer <seth.messer@gmail.com>
2018-10-16 16:12:51 -0500

    Update all.sh

--- asdf/all.sh
+++ asdf/all.sh
@@ -3,7 +3,7 @@
 echo "## asdf-vm..."
 echo ""
 
-# clone asdf-vm (no need for homebrew version of asdf when doing this)
+# clone asdf-vm (no need for homebrew version of asdf if we're doing this)
 # if [[ ! -d "$HOME/.asdf" ]]
 # then
 #   echo ":: ~/.asdf not found; cloning it now.."#
@@ -24,16 +24,10 @@ asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git;
 # https://github.com/asdf-vm/asdf-nodejs#install
 /usr/bin/env zsh $HOME/.asdf/plugins/nodejs/bin/import-release-team-keyring;
 
-#asdf plugin-add ruby
-#asdf plugin-add nodejs
-#asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
-#asdf plugin-add elm https://github.com/vic/asdf-elm.git
-#asdf plugin-add lua https://github.com/Stratus3D/asdf-lua.git
-
-#
-# required for asdf-nodejs..
-# https://github.com/asdf-vm/asdf-nodejs#install
-# sh ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
+# TODO:
+# add python asdf installer.. his notes about multiple python versions and reshim
+# https://github.com/danhper/asdf-python
+# asdf plugin-add python https://github.com/tuvistavie/asdf-python.git
 
 # must initially symlink our tool-versions file for asdf to install the right things..
 ln -sfv $DOTS/asdf/tool-versions.symlink $HOME/.tool-versions
