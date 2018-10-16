137447c35a5ac64468102c62930a0645579aecf7
Seth Messer <seth.messer@gmail.com>
2018-10-16 16:10:05 -0500

    Update node.sh

--- asdf/node.sh
+++ asdf/node.sh
@@ -6,11 +6,11 @@ echo ""
 #echo ":: installing node packages..."
 #$DOTS/node/package-installer
 
-if (( $+commands[yarn] )); then
+if (( which yarn &> /dev/null )); then
   echo ""
   echo ":: attempting to forcefully install neovim-node-host"
   yarn global add neovim # neovim gets angry when trying to use asdf's shim of neovim-node-host
 else
-  echo ":: ERROR: wasn't able to run `yarn` command from ln 12"
+  echo ":: ERROR: wasn't able to run yarn command from ln 12"
 fi
 
