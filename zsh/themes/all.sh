b2dd8aba1315c46dd0a84b27ce7c7f3698b0be97
Seth Messer <seth.messer@gmail.com>
2018-10-16 18:56:13 -0500

    Delete all.sh

--- zsh/themes/all.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zsh
-
-echo "Symlinking custom zsh theme to /usr/local/share/zsh/site-functions"
-
-ln -sfv $DOTS/zsh/themes/prompt_pure.zsh /usr/local/share/zsh/site-functions/prompt_pure_setup
-ln -sfv $DOTS/zsh/themes/async.zsh /usr/local/share/zsh/site-functions/async
-# ln -sfv $DOTS/zsh/themes/gitstatus.zsh /usr/local/share/zsh/site-functions/gitstatus
-# ln -sfv $DOTS/zsh/themes/gitstatus.py /usr/local/share/zsh/site-functions/gitstatus.py
