48ce8fdada422bc15f1ee648e511cf770f90ac34
Seth Messer <seth.messer@gmail.com>
2018-10-16 16:23:00 -0500

    Update all.sh

--- python/all.sh
+++ python/all.sh
@@ -10,4 +10,5 @@ $DOTS/python/package-installer
 
 # this is for wee_slack.py plugin to work for weechat,
 # https://github.com/wee-slack/wee-slack#1-install-dependencies:
+echo ":: installing weechat/wee-slack specific workaround.."
 sudo /usr/local/opt/python@2/bin/pip2 install websocket_client
