2244b3c54897b4971c4146cab261956932bdbda5
Seth Messer <seth.messer@gmail.com>
2018-10-16 16:43:29 -0500

    Update all.sh

--- asdf/all.sh
+++ asdf/all.sh
@@ -17,7 +17,7 @@ source $HOME/.asdf/asdf.sh;
 asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git;
 asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git;
 asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git;
-asdf plugin-add golang https://github.com/kennyp/asdf-golang.git;
+#asdf plugin-add golang https://github.com/kennyp/asdf-golang.git;
 asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git;
 #
 # required for asdf-nodejs..
