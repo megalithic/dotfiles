7feb9186cce2445b065428ec841368d6b02b610a
Seth Messer <seth.messer@gmail.com>
2018-10-16 15:01:10 -0500

    Update node.sh

--- asdf/node.sh
+++ asdf/node.sh
@@ -6,5 +6,7 @@ echo ""
 echo ":: installing node packages..."
 $DOTS/node/package-installer
 
-yarn global add neovim # neovim gets angry when trying to use asdf's shim of neovim-node-host
+if (( $+commands[yarn] )); then
+  yarn global add neovim # neovim gets angry when trying to use asdf's shim of neovim-node-host
+fi
 
