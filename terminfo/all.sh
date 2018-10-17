a2e40aac0f58146c9fcbc2fcee1d42be65ea453b
Seth Messer <seth.messer@gmail.com>
2018-10-16 22:40:45 -0500

    Update all.sh

--- terminfo/all.sh
+++ terminfo/all.sh
@@ -1,8 +1,12 @@
-#!/usr/bin/env sh
+#!/usr/bin/env zsh
+
+echo ""
+echo ":: setting up terminfo related things"
+echo ""
 
 tic -o $HOME/.terminfo $HOME/.dotfiles/terminfo/terminfo.symlink/tmux.terminfo
 tic -o $HOME/.terminfo $HOME/.dotfiles/terminfo/terminfo.symlink/tmux-256color.terminfo
 tic -o $HOME/.terminfo $HOME/.dotfiles/terminfo/terminfo.symlink/xterm-256color.terminfo
 
-echo 'Veriying italics and standouts work..'
+echo ':: veriying italics and standouts work..'
 echo `tput sitm`italics`tput ritm` `tput smso`standout`tput rmso`
