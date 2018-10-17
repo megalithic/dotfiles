e482a0399ae130d4e21e72fe0e2334b8685f0936
Seth Messer <seth.messer@gmail.com>
2018-10-16 22:25:06 -0500

    Update macos.sh

--- elixir/macos.sh
+++ elixir/macos.sh
@@ -13,6 +13,12 @@ wget https://github.com/$(wget https://github.com/jakebecker/elixir-ls/releases/
 unzip elixir-ls.zip
 rm elixir-ls.zip
 
-mix local.hex --force --if-missing
+if (which mix &>/dev/null); then
+  echo ""
+  echo ":: attempting to install elixir/mix/local.hex things"
+  mix local.hex --force --if-missing
+else
+  echo ":: ERROR: wasn't able to run mix command from ln 19"
+fi
 
 chmod +x $HOME/.elixir-ls/language_server.sh
