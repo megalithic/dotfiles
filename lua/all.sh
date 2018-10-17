8d2672011532620e05620aa687b17d22fe1e7769
Seth Messer <seth.messer@gmail.com>
2018-10-16 22:32:25 -0500

    Update all.sh

--- lua/all.sh
+++ lua/all.sh
@@ -2,6 +2,10 @@
 
 # installs lua things
 
+echo ""
+echo ":: setting up lua things..."
+echo ""
+
 luarocks install --server=http://luarocks.org/dev lua-lsp
 luarocks install luacheck
 luarocks install lcf
