e23cd94e4cd4f7cf87d4a7d0a619b3a77b7709eb
Seth Messer <seth.messer@gmail.com>
2018-10-17 21:52:39 -0500

    [node] uses n/avn again

--- node/all.sh
+++ node/all.sh
@@ -3,19 +3,22 @@
 echo ""
 echo ":: setting up node..."
 echo ""
-
-echo ":: installing `n` (my preferred node version manager)..."
-rm -rf $HOME/.n
-rm -rf $HOME/n
+echo ":: installing n (my preferred node version manager)..."
+rm -rf "$HOME/.n"
+rm -rf "$HOME/n"
 rm /usr/local/bin/n
 
 curl -L https://git.io/n-install | bash -s -- -y lts # this installs `n` && node lts
 export N_PREFIX="$HOME/.n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
+
+source "$HOME/.zshrc"
 n lts # always make sure we have lts loaded
 
+echo ""
 echo ":: installing node packages..."
-$DOTS/node/package-installer
+"$DOTS/node/package-installer"
 
+echo ""
 echo ":: setting up `avn` (automatic node version loader)..."
 [[ -s "$HOME/.avn/bin/avn.sh" ]] && source "$HOME/.avn/bin/avn.sh" # source avn
 avn setup
--- zsh/components/zshrc/prompt.zsh
+++ zsh/components/zshrc/prompt.zsh
@@ -1,6 +1,12 @@
 #!/usr/bin/env zsh
 
-# prompt
+# pure'ish prompt
+
+NEWLINE=$'\n'
+PROMPT_SYMBOL="❯"
+PROMPT_VICMD_SYMBOL="❮"
+PROMPT_BACKGROUND_SYMBOL="ﱦ"
+
 setopt prompt_subst
 autoload -U colors && colors
 autoload -U promptinit; promptinit
@@ -8,33 +14,58 @@ autoload -Uz vcs_info
 
 zstyle ':vcs_info:*' enable git svn
 zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:*' check-for-staged-changes true
 zstyle ':vcs_info:git*' formats "[%{$fg[magenta]%}%b%{$reset_color%}%a%m%u%c]"
 zstyle ':vcs_info:*' stagedstr " %{$fg[green]%}●%{$reset_color%}"
 zstyle ':vcs_info:*' unstagedstr " %{$fg[red]%}✚%{$reset_color%}"
+
 precmd() {
   vcs_info
 }
 
-# highlights the timestamp on error
-function check_last_exit_code() {
-  local LAST_EXIT_CODE=$?
-  local EXIT_CODE_PROMPT=' '
-  if [[ $LAST_EXIT_CODE -ne 0 ]]; then
-    EXIT_CODE_PROMPT+="%{$fg[red]%}(%{$reset_color%}"
-    EXIT_CODE_PROMPT+="%{$fg_bold[red]%}$LAST_EXIT_CODE%{$reset_color%}"
-    EXIT_CODE_PROMPT+="%{$fg[red]%}) %t%{$reset_color%}"
+# highlights the prompt_symbol on error
+exit_code_prompt_symbol() {
+  local last_exit_code=$?
+  local exit_code_prompt=''
+
+  if [[ $last_exit_code -ne 0 ]]; then
+    exit_code_prompt+="%{$fg_bold[red]%}$PROMPT_SYMBOL%{$reset_color%}"
   else
-    EXIT_CODE_PROMPT+="%{$fg[green]%}%t%{$reset_color%}"
+    exit_code_prompt+="%{$fg[green]%}$PROMPT_SYMBOL%{$reset_color%}"
   fi
 
-  echo "$EXIT_CODE_PROMPT"
+  echo "$exit_code_prompt"
 }
 
-_newline=$'\n'
-_lineup=$'\e[1A'
-_linedown=$'\e[1B'
+# returns a more preferred truncated path
+prompt_path() {
+  local prompt_path=''
+  local pwd="${PWD/#$HOME/~}"
+  if [[ "$pwd" == (#m)[/~] ]]; then
+    prompt_path="~"
+  else
+    prompt_path="%{$fg[blue]%}${${${(@j:/:M)${(@s:/:)pwd}##.#?}:h}%/}/${pwd:t}%{$reset_color%}"
+  fi
 
-PROMPT_SYMBOL="❯"
+  echo "$prompt_path"
+}
 
-PROMPT='${_newline}%{$fg[blue]%}%2/%{$reset_color%} ${vcs_info_msg_0_}${_newline}${PROMPT_SYMBOL} '
-# RPROMPT='%{${_lineup}%}$(check_last_exit_code)%{${_linedown}%}'
+# returns a fancy indicator when there are running background jobs
+background_process_indicator() {
+  local background_indicator=''
+  [[ $(jobs -l | wc -l) -gt 0 ]] && background_indicator="$PROMPT_BACKGROUND_SYMBOL"
+
+  echo "$background_indicator"
+}
+
+# TODO: alter the PURE_PROMPT_SYMBOL when we're in normal mode vs. insert mode
+# build_prompt_symbol() {
+#   # prompt_parts+=(${${KEYMAP/vicmd/${PURE_PROMPT_VICMD_SYMBOL:-❮}}/(main|viins)/${PURE_PROMPT_SYMBOL:-❯}})
+#   prompt_parts+="${PROMPT_SYMBOL}"
+# }
+
+# construct our prompt
+PROMPT='${NEWLINE}'
+PROMPT+='$(prompt_path) ${vcs_info_msg_0_} $(background_process_indicator)'
+PROMPT+='${NEWLINE}'
+PROMPT+='$(exit_code_prompt_symbol) '
