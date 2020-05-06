fe40b48c17834e00f97c87cd40a30ddc8d9f1e24
Seth Messer <seth.messer@gmail.com>
2020-05-06 09:52:48 -0500

    Fix an asdf thing

--- zsh/components/zshrc/asdf.zsh
+++ zsh/components/zshrc/asdf.zsh
@@ -1,2 +1,5 @@
 source $HOME/.asdf/asdf.sh
-source $HOME/.asdf/completions/asdf.bash
+
+# NOT needed anymore --
+# https://github.com/asdf-vm/asdf/issues/68#issuecomment-229237561
+# source $HOME/.asdf/completions/asdf.bash
