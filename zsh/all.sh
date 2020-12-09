2fac709c46f096775e6dacf9c32ff44d381207a7
Seth Messer <seth.messer@gmail.com>
2020-12-09 14:02:15 -0600

    more updates to update scripts

--- zsh/all.sh
+++ zsh/all.sh
@@ -4,8 +4,10 @@ echo ""
 echo ":: setting up zsh related things"
 echo ""
 
+echo "-> setting zsh dir symlinks"
 ln -sfv $DOTS/zsh $HOME/.zsh
-ln -sfv $DOTS/zsh $HOME/.config/zsh
+ln -sfv $DOTS/zsh $HOME/.config/
 
 # to hopefully pull down what we need for the gitstatus plugin submodule
+echo "-> fetching gitstatus submodule"
 cd $DOTS/zsh/plugins/gitstatus && git pull && git submodule update --init --recursive && cd -
