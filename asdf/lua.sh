775ff309688230e72230d84daf2c70e0c5e77bca
Seth Messer <seth.messer@gmail.com>
2020-12-09 13:44:02 -0600

    adds checks before re-intsalling all the lua things agian

--- asdf/lua.sh
+++ asdf/lua.sh
@@ -7,10 +7,21 @@ if (which luarocks &>/dev/null); then
   echo ""
   echo ":: attempting to install luarocks related tings"
 
+if (which lua-lsp &>/dev/null); then
   luarocks install --server=http://luarocks.org/dev lua-lsp
-  luarocks install luacheck
-  luarocks install lcf
-  luarocks install --server=https://luarocks.org/dev luaformatter
+fi
+
+if (which luacheck &>/dev/null); then
+    luarocks install luacheck
+fi
+
+if (which lcf &>/dev/null); then
+    luarocks install lcf
+fi
+
+if (which luaformatter &>/dev/null); then
+    luarocks install --server=https://luarocks.org/dev luaformatter
+fi
 else
   echo ":: ERROR: unable to run luarocks command from ln 12-14; likely luarocks isn't available"
 fi
