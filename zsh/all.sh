760d3af211c97d7f84c0f5235f6403bc8901edc3
Seth Messer <seth.messer@gmail.com>
2020-12-09 13:56:51 -0600

    make sure we get the submodule for gitstatus

--- zsh/all.sh
+++ zsh/all.sh
@@ -6,3 +6,6 @@ echo ""
 
 ln -sfv $DOTS/zsh $HOME/.zsh
 ln -sfv $DOTS/zsh $HOME/.config/zsh
+
+# to hopefully pull down what we need for the gitstatus plugin submodule
+cd $DOTS/zsh/plugins/gitstatus && git pull && git submodule update --init --recursive && cd -
