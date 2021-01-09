e8234ee160c9c9ed8d9970012f9bae4d8d715154
Seth Messer <seth.messer@gmail.com>
2021-01-08 22:24:39 -0600

    more linux speific install

--- platform/linux.sh
+++ platform/linux.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env zsh
 
+log "-> linux-specific configuration.."
+
 echo ""
 echo ":: setting up linux system related things"
 echo ""
 
-echo "nothing to setup yet.."
+# we're on a familiar distro (debian based)
+if [ -f "/etc/debian_version" ]; then
+  # install some deps..
+  sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
+
+  # install neovim nightly please..
+  # REF: https://dev.to/creativenull/installing-neovim-nightly-alongside-stable-10d0
+  mkdir -p $HOME/builds
+  git clone https://github.com/neovim/neovim.git $HOME/builds/neovim
+  cd $HOME/builds/neovim
+  make CMAKE_BUILD_TYPE=Release
+  cd -
+fi
