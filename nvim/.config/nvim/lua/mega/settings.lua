890a7d08aa0a5edc165dff1ce544f8b98e14e836
Seth Messer <seth.messer@gmail.com>
2021-01-21 19:49:59 -0600

    no longer needed custom rg thingy

--- nvim/.config/nvim/lua/mega/settings.lua
+++ nvim/.config/nvim/lua/mega/settings.lua
@@ -80,19 +80,6 @@ vim.g.fzf_preview_window = {"right:50%", "alt-p"}
 mega.map("n", "<Leader>m", "<cmd>FzfFiles<CR>")
 mega.map("n", "<Leader>a", "<cmd>FzfRg<CR>")
 mega.map("n", "<Leader>A", "<ESC>:exe('FzfRg '.expand('<cword>'))<CR>")
-vim.api.nvim_exec(
-  [[
-    function! RipgrepFzf(query, fullscreen)
-      let command_fmt = 'rg --column --line-number --no-heading --color=always --smart-case -- %s || true'
-      let initial_command = printf(command_fmt, shellescape(a:query))
-      let reload_command = printf(command_fmt, '{q}')
-      let spec = {'options': ['--phony', '--query', a:query, '--bind', 'change:reload:'.reload_command]}
-      call fzf#vim#grep(initial_command, 1, fzf#vim#with_preview(spec), a:fullscreen)
-    endfunction
-    command! -nargs=* -bang Rg call RipgrepFzf(<q-args>, <bang>0)
-    ]],
-  true
-)
 
 -- vim-polyglot
 vim.g.polyglot_disabled = {
