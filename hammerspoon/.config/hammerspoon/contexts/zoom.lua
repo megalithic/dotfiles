3f05438de1a999fe438cebf5e9f12d204ab69878
Seth Messer <seth.messer@gmail.com>
2021-08-12 09:00:25 -0500

    [efm,hs] disable shellcheck and fix window sizing for zoom/zetty context

--- hammerspoon/.config/hammerspoon/contexts/zoom.lua
+++ hammerspoon/.config/hammerspoon/contexts/zoom.lua
@@ -1,5 +1,5 @@
 local cache = {}
-local M = {cache = cache}
+local M = { cache = cache }
 
 local fn = require("hs.fnutils")
 local wh = require("utils.wm.window-handlers")
@@ -10,78 +10,69 @@ local init_apply_complete = false
 
 -- apply(string, hs.application, hs.logger) :: nil
 M.apply = function(event, app, log)
-  -- prevents excessive actions on multiple window creations
-  if not init_apply_complete then
-    if fn.contains({"windowCreated", hs.application.watcher.launched}, event) or app:isRunning() then
-      ----------------------------------------------------------------------
-      -- handle DND toggling
-      wh.dndHandler(app, {enabled = true, mode = "zoom"}, event)
+	-- prevents excessive actions on multiple window creations
+	if not init_apply_complete then
+		if fn.contains({ "windowCreated", hs.application.watcher.launched }, event) or app:isRunning() then
+			----------------------------------------------------------------------
+			-- handle DND toggling
+			wh.dndHandler(app, { enabled = true, mode = "zoom" }, event)
 
-      ----------------------------------------------------------------------
-      -- pause spotify
-      spotify("pause")
+			----------------------------------------------------------------------
+			-- pause spotify
+			spotify("pause")
 
-      ----------------------------------------------------------------------
-      -- mute (PTT) by default
-      ptt.setState("push-to-talk")
+			----------------------------------------------------------------------
+			-- mute (PTT) by default
+			ptt.setState("push-to-talk")
 
-      ----------------------------------------------------------------------
-      -- close web browser "zoom launching" tabs
-      browser.killTabsByDomain("zoom.us")
+			----------------------------------------------------------------------
+			-- close web browser "zoom launching" tabs
+			browser.killTabsByDomain("zoom.us")
 
-      do
-        local zoom = hs.application.get("zoom.us")
-        local kitty = hs.application.get("kitty")
+			do
+				local zoom = hs.application.get("zoom.us")
+				local kitty = hs.application.get("kitty")
 
-        hs.timer.waitUntil(
-          function()
-            return zoom:getWindow("Zoom Meeting")
-          end,
-          function()
-            local target_close_window = zoom:getWindow("Zoom")
-            if target_close_window ~= nil then
-              target_close_window:close()
-            end
+				hs.timer.waitUntil(function()
+					return zoom:getWindow("Zoom Meeting")
+				end, function()
+					local target_close_window = zoom:getWindow("Zoom")
+					if target_close_window ~= nil then
+						target_close_window:close()
+					end
 
-            local layouts = {
-              {"zoom.us", "Zoom Meeting", hs.screen.primaryScreen():name(), hs.layout.left70, nil, nil},
-              {"kitty", nil, hs.screen.primaryScreen():name(), hs.layout.right30, nil, nil}
-            }
-            hs.layout.apply(layouts)
-            kitty:setFrontmost(true)
+					local layouts = {
+						{ "zoom.us", "Zoom Meeting", hs.screen.primaryScreen():name(), hs.layout.left50, nil, nil },
+						{ "kitty", nil, hs.screen.primaryScreen():name(), hs.layout.right50, nil, nil },
+					}
+					hs.layout.apply(layouts)
+					kitty:setFrontmost(true)
 
-            hs.timer.doAfter(
-              0.5,
-              function()
-                hs.execute(os.getenv("HOME") .. "/.dotfiles/bin/zetty meeting", true)
-              end
-            )
-          end
-        )
-      end
-    end
+					hs.timer.doAfter(0.5, function()
+						hs.execute(os.getenv("HOME") .. "/.dotfiles/bin/zetty meeting", true)
+					end)
+				end)
+			end
+		end
 
-    init_apply_complete = true
-  end
+		init_apply_complete = true
+	end
 
-  ----------------------------------------------------------------------
-  -- things to do on app exit
-  wh.onAppQuit(
-    app,
-    function()
-      local kitty = hs.application.get("kitty")
-      -- FIXME: do i really need all the error checking here?
-      if kitty ~= nil then
-        local kitty_win = kitty:mainWindow()
-        if kitty_win ~= nil then
-          kitty_win:moveToUnit(hs.layout.maximized)
-        end
-      end
+	----------------------------------------------------------------------
+	-- things to do on app exit
+	wh.onAppQuit(app, function()
+		local kitty = hs.application.get("kitty")
+		-- FIXME: do i really need all the error checking here?
+		if kitty ~= nil then
+			local kitty_win = kitty:mainWindow()
+			if kitty_win ~= nil then
+				kitty_win:moveToUnit(hs.layout.maximized)
+			end
+		end
 
-      ptt.setState("push-to-talk")
-      init_apply_complete = false
-    end
-  )
+		ptt.setState("push-to-talk")
+		init_apply_complete = false
+	end)
 end
 
 return M
--- nvim/.config/nvim/lua/efm.lua
+++ nvim/.config/nvim/lua/efm.lua
@@ -1,19 +1,19 @@
 local vint = {
-  lintCommand = "vint -",
-  lintStdin = true,
-  lintFormats = {"%f:%l:%c: %m"}
+	lintCommand = "vint -",
+	lintStdin = true,
+	lintFormats = { "%f:%l:%c: %m" },
 }
 local luafmt = {
-  formatCommand = "luafmt --indent-count 2 --stdin",
-  -- formatCommand = "luafmt ${-i:tabWidth} --stdin",
-  formatStdin = true
+	formatCommand = "luafmt --indent-count 2 --stdin",
+	-- formatCommand = "luafmt ${-i:tabWidth} --stdin",
+	formatStdin = true,
 }
 local mix_credo = {
-  lintCommand = "mix credo suggest --format=flycheck --read-from-stdin ${INPUT}",
-  lintStdin = true,
-  lintIgnoreExitCode = true,
-  lintFormats = {"%f:%l:%c: %m"},
-  rootMarkers = {"mix.lock"} -- for some reason, only mix.lock works in vpp
+	lintCommand = "mix credo suggest --format=flycheck --read-from-stdin ${INPUT}",
+	lintStdin = true,
+	lintIgnoreExitCode = true,
+	lintFormats = { "%f:%l:%c: %m" },
+	rootMarkers = { "mix.lock" }, -- for some reason, only mix.lock works in vpp
 }
 -- local golint = require "mega.lc.efm.golint"
 -- local goimports = require "mega.lc.efm.goimports"
@@ -21,81 +21,69 @@ local mix_credo = {
 -- local isort = require "mega.lc.efm.isort"
 -- local flake8 = require "mega.lc.efm.flake8"
 -- local mypy = require "mega.lc.efm.mypy"
-local stylua = {formatCommand = "stylua -", formatStdin = true}
+local stylua = { formatCommand = "stylua -", formatStdin = true }
 local selene = {
-  lintCommand = "selene --display-style quiet -",
-  lintIgnoreExitCode = true,
-  lintStdin = true,
-  lintFormats = {"%f:%l:%c: %tarning%m", "%f:%l:%c: %tarning%m"}
+	lintCommand = "selene --display-style quiet -",
+	lintIgnoreExitCode = true,
+	lintStdin = true,
+	lintFormats = { "%f:%l:%c: %tarning%m", "%f:%l:%c: %tarning%m" },
 }
-
 local prettierLocal = {
-  formatCommand = "./node_modules/.bin/prettier --stdin --stdin-filepath ${INPUT}",
-  formatStdin = true
+	formatCommand = "./node_modules/.bin/prettier --stdin --stdin-filepath ${INPUT}",
+	formatStdin = true,
 }
-
 local prettierGlobal = {
-  formatCommand = "prettier --stdin --stdin-filepath ${INPUT}",
-  formatStdin = true
+	formatCommand = "prettier --stdin --stdin-filepath ${INPUT}",
+	formatStdin = true,
 }
-
 local eslint = {
-  lintCommand = "eslint_d -f visualstudio --stdin --stdin-filename ${INPUT}",
-  lintIgnoreExitCode = true,
-  lintStdin = true,
-  lintFormats = {"%f(%l,%c): %tarning %m", "%f(%l,%c): %trror %m"}
+	lintCommand = "eslint_d -f visualstudio --stdin --stdin-filename ${INPUT}",
+	lintIgnoreExitCode = true,
+	lintStdin = true,
+	lintFormats = { "%f(%l,%c): %tarning %m", "%f(%l,%c): %trror %m" },
 }
-
 local shellcheck = {
-  lintCommand = "shellcheck -f gcc -x -",
-  lintStdin = true,
-  lintFormats = {"%f=%l:%c: %trror: %m", "%f=%l:%c: %tarning: %m", "%f=%l:%c: %tote: %m"}
+	lintCommand = "shellcheck -f gcc -x -",
+	lintStdin = true,
+	lintFormats = { "%f=%l:%c: %trror: %m", "%f=%l:%c: %tarning: %m", "%f=%l:%c: %tote: %m" },
 }
-
 local markdownlint = {
-  lintCommand = "markdownlint -s",
-  lintStdin = true,
-  lintFormats = {"%f:%l:%c %m"}
+	lintCommand = "markdownlint -s",
+	lintStdin = true,
+	lintFormats = { "%f:%l:%c %m" },
 }
-
-local fish = {formatCommand = "fish_indent", formatStdin = true}
-
-local eslintPrettier = {prettierGlobal, eslint}
-local shellcheck = {
-  lintCommand = "shellcheck -f gcc -x -",
-  lintStdin = true,
-  lintFormats = {"%f:%l:%c: %trror: %m", "%f:%l:%c: %tarning: %m", "%f:%l:%c: %tote: %m"}
-}
+local fish = { formatCommand = "fish_indent", formatStdin = true }
 local misspell = {
-  lintCommand = "misspell",
-  lintIgnoreExitCode = true,
-  lintStdin = true,
-  lintFormats = {"%f:%l:%c: %m"}
+	lintCommand = "misspell",
+	lintIgnoreExitCode = true,
+	lintStdin = true,
+	lintFormats = { "%f:%l:%c: %m" },
 }
 local shfmt = {
-  formatCommand = "shfmt -ci -s -bn",
-  formatStdin = true
+	formatCommand = "shfmt -ci -s -bn",
+	formatStdin = true,
 }
+local eslintPrettier = { prettierGlobal, eslint }
 
 return {
-  -- ["="] = {misspell},
-  vim = {vint},
-  lua = {stylua},
-  elixir = {mix_credo},
-  eelixir = {mix_credo},
-  -- go = {golint, goimports},
-  -- python = {black, isort, flake8, mypy},
-  typescript = eslintPrettier,
-  javascript = eslintPrettier,
-  typescriptreact = eslintPrettier,
-  javascriptreact = eslintPrettier,
-  yaml = eslintPrettier,
-  json = eslintPrettier,
-  html = eslintPrettier,
-  scss = eslintPrettier,
-  css = eslintPrettier,
-  markdown = eslintPrettier,
-  sh = {shellcheck, shfmt},
-  zsh = {shellcheck, shfmt}
-  -- tf = {terraform},
+	-- ["="] = {misspell},
+	vim = { vint },
+	lua = { stylua },
+	elixir = { mix_credo },
+	eelixir = { mix_credo },
+	-- go = {golint, goimports},
+	-- python = {black, isort, flake8, mypy},
+	typescript = eslintPrettier,
+	javascript = eslintPrettier,
+	typescriptreact = eslintPrettier,
+	javascriptreact = eslintPrettier,
+	yaml = eslintPrettier,
+	json = eslintPrettier,
+	html = eslintPrettier,
+	scss = eslintPrettier,
+	css = eslintPrettier,
+	markdown = eslintPrettier,
+	sh = { shellcheck, shfmt },
+	zsh = { shfmt },
+	-- tf = {terraform},
 }
