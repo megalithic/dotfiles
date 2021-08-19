4d316a1af8a4294a2aef51ff55e16f7bd727539e
Seth Messer <seth.messer@gmail.com>
2021-08-19 16:54:37 -0500

    megaforest pls

--- nvim/.config/nvim/lua/colors.lua
+++ nvim/.config/nvim/lua/colors.lua
@@ -3,7 +3,7 @@ local hi, link, utf8 = mega.hi, mega.hi_link, mega.utf8
 
 local hsl = require("lush").hsl
 
-local theme = "everforest"
+local theme = "megaforest"
 
 local icons = {
 	sign_error = utf8(0xf655),
