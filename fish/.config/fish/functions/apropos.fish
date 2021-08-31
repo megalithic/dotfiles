f3a71c5e99bba83a6eac47f245c3d067431a7559
Seth Messer <seth.messer@gmail.com>
2021-08-30 22:22:05 -0500

    tweaks.. maybe?

--- fish/.config/fish/functions/apropos.fish
+++ fish/.config/fish/functions/apropos.fish
@@ -1,3 +1,3 @@
-function apropos --description 'alias apropos MANPATH=/Users/sethmesser/.cache/fish command apropos'
-  MANPATH=/Users/sethmesser/.cache/fish command apropos $argv; 
+function apropos --description 'alias apropos MANPATH=/Users/seth/.cache/fish command apropos'
+  MANPATH=/Users/seth/.cache/fish command apropos $argv; 
 end
