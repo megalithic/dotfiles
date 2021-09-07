bf781121eacbe3161b3da42aaf50e6b120bf2924
Seth Messer <seth.messer@gmail.com>
2021-09-07 16:42:52 -0500

    chore(): updates to dotup, linux platform setup, brewfiles

--- platform/linux.sh
+++ platform/linux.sh
@@ -13,20 +13,28 @@ if [ -f "/etc/debian_version" ]; then
 	sudo apt-get update
 	sudo apt-get -y install linux-headers-$(uname -r) build-essential autoconf m4 libncurses5-dev libwxgtk3.0-gtk3-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev xsltproc fop libxml2-utils libncurses-dev openjdk-11-jdk ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip bat xclip lib32readline-dev libreadline-dev dirmngr gpg curl fd-find fish zsh exa silversearcher-ag shellcheck ripgrep libgsl-dev && log_ok "DONE installing linux deps" || log_error "failed to install linux deps"
 
-	log "installing starship prompt"
-	sh -c "$(curl -fsSL https://starship.rs/install.sh)" && log_ok "DONE installing starship" || log_error "failed to install starship"
+	if (command -v fzf &>/dev/null); then
+		log "starship prompt already installed; skipping."
+	else
+		log_warn "starship prompt not found; installing.."
+		sh -c "$(curl -fsSL https://starship.rs/install.sh)" && log_ok "DONE installing starship" || log_error "failed to install starship"
+	fi
 
-	log "installing gitstatus for zsh"
-	git clone --depth=1 https://github.com/romkatv/gitstatus.git "$builds_path/gitstatus"
+	# log "installing gitstatus for zsh"
+	# git clone --depth=1 https://github.com/romkatv/gitstatus.git "$builds_path/gitstatus"
 
 	log "installing zsh addons"
 	git clone https://github.com/zsh-users/zsh-autosuggestions.git "$builds_path/zsh-autosuggestions"
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$builds_path/zsh-syntax-highlighting"
 	git clone https://github.com/zsh-users/zsh-history-substring-search "$builds_path/zsh-history-substring-search"
 
-	log "installing fzf"
-	git clone --depth 1 https://github.com/junegunn/fzf.git "$builds_path/fzf"
-	$builds_path/fzf/install
+	if (command -v fzf &>/dev/null); then
+		log "fzf already installed; skipping."
+	else
+		log_warn "fzf not found; installing.."
+		git clone --depth 1 https://github.com/junegunn/fzf.git "$builds_path/fzf"
+		$builds_path/fzf/install
+	fi
 
 	log "installing neovim nightly"
 	# REF: https://dev.to/creativenull/installing-neovim-nightly-alongside-stable-10d0
