4d7019eb7e7c33fc1c5bd12c02c3175ae35687e8
Seth Messer <seth.messer@gmail.com>
2021-10-01 10:29:59 -0500

    adds eruby thing

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -8,15 +8,6 @@
 -- plugin [All | Ran at startup or packadd]
 -- syntax [All]
 
--- vim.cmd([[
--- " Intelligently navigate tmux panes and Vim splits using the same keys.
--- " See https://sunaku.github.io/tmux-select-pane.html for documentation.
-
--- let progname = substitute($VIM, '.*[/\\]', '', '')
--- set title titlestring=%{progname}\ %f\ +%l\ #%{tabpagenr()}.%{winnr()}
--- if &term =~ '^screen' && !has('nvim') | exe "set t_ts=\e]2; t_fs=\7" | endif
--- ]])
-
 _G["mega"] = require("global")
 
 local load = mega.load
