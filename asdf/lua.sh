a3572c0a5522e134277ebee7572950610692f828
Seth Messer <seth.messer@gmail.com>
2022-01-28 10:08:35 -0600

    chore(install): tweaks; default to N for addons

--- asdf/lua.sh
+++ asdf/lua.sh
@@ -5,28 +5,28 @@
 set -euo pipefail
 
 do_install() {
-	if (has luarocks); then
-		(! has luacheck) && luarocks install luacheck
-		(! has lcf) && luarocks install lcf
-		# (! has lua-lsp) && luarocks install --server=https://luarocks.org/dev lua-lsp
-		(! has lua-format) && luarocks install --server=https://luarocks.org/dev luaformatter
-		(! has lua-nuspell) && luarocks install lua-nuspell
-	fi
+  if (has luarocks); then
+    (! has luacheck) && luarocks install luacheck
+    (! has lcf) && luarocks install lcf
+    # (! has lua-lsp) && luarocks install --server=https://luarocks.org/dev lua-lsp
+    (! has lua-format) && luarocks install --server=https://luarocks.org/dev luaformatter
+    (! has lua-nuspell) && luarocks install lua-nuspell
+  fi
 }
 
-read -p "$(tput bold)$(tput setaf 5)[?] download and install lua addons (Y/n)?$(tput sgr 0) " yn
+read -p "$(tput bold)$(tput setaf 5)[?] download and install lua addons (y/N)?$(tput sgr 0) " yn
 case $yn in
-	[Yy]*)
-		do_install || exit 1
-		;;
-	"")
-		do_install || exit 1
-		;;
-	[Nn]*)
-		log_warn "opted out of installing lua addons"
-		;;
-	*)
-		log_warn "please answer [y]es or [n]o."
-		exec $__invocation
-		;;
+  [Yy]*)
+    do_install || exit 1
+    ;;
+  [Nn]*)
+    log_warn "opted out of installing lua addons"
+    ;;
+  "")
+    log_warn "opted out of installing lua addons"
+    ;;
+  *)
+    log_warn "please answer [y]es or [n]o."
+    exec $__invocation
+    ;;
 esac
--- asdf/rust.sh
+++ asdf/rust.sh
@@ -5,40 +5,40 @@
 set -euo pipefail
 
 do_install() {
-	# -- handling the install of rust with asdf
-	# - or -
-	# -- handle with direct install via rustup
-	# if (has rustup); then
-	#   log "installing rustup"
-	# 	rustup install stable
-	# 	rustup default stable
-	# fi
+  # -- handling the install of rust with asdf
+  # - or -
+  # -- handle with direct install via rustup
+  # if (has rustup); then
+  #   log "installing rustup"
+  # 	rustup install stable
+  # 	rustup default stable
+  # fi
 
-	if (has cargo); then
-		# (! has luacheck) && luarocks install luacheck
-		cargo install selene  # https://kampfkarren.github.io/selene/selene.html
-		cargo install stylua  # https://github.com/johnnymorganz/stylua
-		cargo install distant # https://github.com/chipsenkbeil/distant
+  if (has cargo); then
+    # (! has luacheck) && luarocks install luacheck
+    cargo install selene  # https://kampfkarren.github.io/selene/selene.html
+    cargo install stylua  # https://github.com/johnnymorganz/stylua
+    cargo install distant # https://github.com/chipsenkbeil/distant
 
-		if [[ $PLATFORM == "linux" ]]; then
-			cargo install git-delta
-		fi
-	fi
+    if [[ $PLATFORM == "linux" ]]; then
+      cargo install git-delta
+    fi
+  fi
 }
 
-read -p "$(tput bold)$(tput setaf 5)[?] download and install rust addons (Y/n)?$(tput sgr 0) " yn
+read -p "$(tput bold)$(tput setaf 5)[?] download and install rust addons (y/N)?$(tput sgr 0) " yn
 case $yn in
-	[Yy]*)
-		do_install || exit 1
-		;;
-	"")
-		do_install || exit 1
-		;;
-	[Nn]*)
-		log_warn "opted out of installing rust addons"
-		;;
-	*)
-		log_warn "please answer [y]es or [n]o."
-		exec $__invocation
-		;;
+  [Yy]*)
+    do_install || exit 1
+    ;;
+  [Nn]*)
+    log_warn "opted out of installing rust addons"
+    ;;
+  "")
+    log_warn "opted out of installing rust addons"
+    ;;
+  *)
+    log_warn "please answer [y]es or [n]o."
+    exec $__invocation
+    ;;
 esac
