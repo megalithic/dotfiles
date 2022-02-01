67c5a17bbe0094db2cf3546dc3fa1ea7bfb86259
Seth Messer <seth.messer@gmail.com>
2022-02-01 10:41:51 -0600

    chore(ts): enables markdown ts; enables ts regex

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -325,7 +325,7 @@ M.setup = function()
         "html",
         "heex",
         "javascript",
-        -- "markdown",
+        "markdown",
         "jsdoc",
         "json",
         "jsonc",
@@ -352,7 +352,7 @@ M.setup = function()
         -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
         -- Using this option may slow down your editor, and you may see some duplicate highlights.
         -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
+        additional_vim_regex_highlighting = true,
       },
       indent = { enable = true },
       autotag = { enable = true },
