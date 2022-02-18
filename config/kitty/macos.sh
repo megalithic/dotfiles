f99755f36c07f81152ce97e0938b7162d1068883
Seth Messer <seth.messer@gmail.com>
2022-02-18 15:47:47 -0600

    chore: update kitty icon; thanks @ryansch

--- config/kitty/macos.sh
+++
@@ -1,6 +0,0 @@
-# https://github.com/DinkDonk/kitty-icon#installation
-cp ./kitty-dark.icns /Applications/kitty.app/Contents/Resources/kitty.icns
-
-rm /var/folders/*/*/*/com.apple.dock.iconcache
-
-killall Dock
---
+++ config/kitty/update_icon.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+function __debug_info() {
+  # -- set some useful vars for executable info:
+  __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+  __file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+  __base="$(basename "${__file}" .sh)"
+  __root="$(cd "$(dirname "${__dir}")" && pwd)"
+  # shellcheck disable=SC2034,SC2015
+  __invocation="$(printf %q "${__file}")$( (($#)) && printf ' %q' "$@" || true)"
+
+  printf -- "%s executation details:\n" "$__base"
+  printf -- "---------------------------------------------------\n"
+  printf -- "cwd:          %s\n" "$__dir"
+  printf -- "filepath:     %s\n" "$__file"
+  printf -- "basename:     %s\n" "$__base"
+  printf -- "root:         %s\n" "$__root"
+  printf -- "invocation:   %s\n" "$__invocation"
+  printf -- "\n"
+}
+# __debug_info
+
+# HOWTO:
+# - https://www.sethvargo.com/replace-icons-osx
+# - https://github.com/DinkDonk/kitty-icon#installation
+cp $DOTS/config/kitty/kitty.icns /Applications/kitty.app/Contents/Resources/kitty.icns
+rm /var/folders/*/*/*/com.apple.dock.iconcache
+touch /Applications/kitty.app
+killall Dock && killall Finder
