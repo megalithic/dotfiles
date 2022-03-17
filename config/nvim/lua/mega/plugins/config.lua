eed75e9075380c263a9545787581b841458f91c6
Seth Messer <seth.messer@gmail.com>
2022-03-17 08:15:30 -0500

    chore(weechat)

--- config/nvim/lua/mega/plugins/config.lua
+++ config/nvim/lua/mega/plugins/config.lua
@@ -443,7 +443,7 @@ do -- nvim-autopairs
   -- https://github.com/rafamadriz/NeoCode/blob/main/lua/modules/plugins/completion.lua#L130-L192
 end
 
-do -- lightspeed.nvim or hop.nvim
+do -- lightspeed.nvim or hop.nvim; testing them both out
   if false then
     vim.cmd("packadd lightspeed.nvim")
     require("lightspeed").setup({
@@ -451,7 +451,9 @@ do -- lightspeed.nvim or hop.nvim
       -- jump_on_partial_input_safety_timeout = 400,
       -- This can get _really_ slow if the window has a lot of content,
       -- turn it on only if your machine can always cope with it.
-      jump_to_unique_chars = true,
+      -- jump_to_unique_chars = true,
+      jump_to_unique_chars = false,
+      safe_labels = {},
       -- grey_out_search_area = true,
       match_only_the_start_of_same_char_seqs = true,
       limit_ft_matches = 5,
--- config/weechat/python/fzf.py
+++ config/weechat/python/fzf.py
@@ -1,32 +1,41 @@
 from typing import Iterator, Tuple
 import weechat
 
-
 SCRIPT_NAME = "fzf"
 SCRIPT_AUTHOR = "Trygve Aaberge <trygveaa@gmail.com>"
 SCRIPT_VERSION = "0.1.0"
 SCRIPT_LICENSE = "MIT"
-SCRIPT_DESC = "Switch buffer using fzf (currently only works inside tmux)"
+SCRIPT_DESC = "Switch buffer using fzf (currently only works inside tmux and kitty)"
 REPO_URL = "https://github.com/trygveaa/weechat-fzf"
 
+def print_log(message: str) -> None:
+    weechat.prnt("", weechat.prefix("debug") + message)
 
 def print_error(message: str) -> None:
     weechat.prnt("", weechat.prefix("error") + message)
+
+def print_warn(message: str) -> None:
+    weechat.prnt("", weechat.prefix("warning") + message)
 
 
 def fzf_process_cb(
     data: str, command: str, return_code: int, out: str, err: str
 ) -> int:
+    print_log("Debugging.. data: {}".format(data))
+    print_log("Debugging.. command: {}".format(command))
+
     if return_code == weechat.WEECHAT_HOOK_PROCESS_ERROR or return_code == 2 or err:
         print_error("Error running fzf (code {}): {}".format(return_code, err))
         return weechat.WEECHAT_RC_OK
     if out != "":
+        print_log("Debugging.. out: {}".format(out))
+
         pointer, _ = out.split("\t", 1)
         weechat.buffer_set(pointer, "display", "1")
     return weechat.WEECHAT_RC_OK
 
 
-def fzf_command_cb(data: str, buffer: str, args: str) -> int:
+def fzf_tmux_command_cb(data: str, buffer: str, args: str) -> int:
     cmd = (
         "fzf-tmux -- --delimiter='\t' --with-nth=3.. "
         "--preview='tail -$LINES {2} 2>/dev/null'"
@@ -37,6 +46,19 @@ def fzf_command_cb(data: str, buffer: str, args: str) -> int:
     weechat.hook_set(hook, "stdin_close", "")
     return weechat.WEECHAT_RC_OK
 
+def fzf_kitty_command_cb(data: str, buffer: str, args: str) -> int:
+        # "kitty @ launch --type=overlay --keep-focus --hold --stdin-source=@last_cmd_output --stdin-add-formatting--stdin-add-line-wrap-markers \""
+    cmd = (
+        "kitty @ launch --type=overlay --keep-focus --hold "
+        "fzf -- --delimiter='\t' --with-nth=3.. "
+        "--preview='tail -$LINES {2} 2>/dev/null'"
+    )
+    hook = weechat.hook_process_hashtable(cmd, {"stdin": "1"}, 0, "fzf_process_cb", "")
+    print_log("Debugging.. hook: {}".format(hook))
+    for buffer_info in buffers():
+        weechat.hook_set(hook, "stdin", "\t".join(buffer_info) + "\n")
+    weechat.hook_set(hook, "stdin_close", "")
+    return weechat.WEECHAT_RC_OK
 
 def buffers() -> Iterator[Tuple[str, str, str, str]]:
     logger_filenames = {}
@@ -55,7 +77,6 @@ def buffers() -> Iterator[Tuple[str, str, str, str]]:
         yield (pointer, logger_filenames.get(pointer, ""), str(number), name)
     weechat.infolist_free(buffer_infolist)
 
-
 def main() -> None:
     if not weechat.register(
         SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT_DESC, "", ""
@@ -63,12 +84,13 @@ def main() -> None:
         return
 
     tmux = weechat.string_eval_expression("${env:TMUX}", {}, {}, {})
-    if not tmux:
-        print_error("Error: fzf.py currently only supports being run inside tmux")
-        return
-
-    weechat.hook_command(SCRIPT_NAME, SCRIPT_DESC, "", "", "", "fzf_command_cb", "")
-
+    kitty = weechat.string_eval_expression("${env:KITTY_LISTEN_ON}", {}, {}, {})
+    if tmux:
+        weechat.hook_command(SCRIPT_NAME, SCRIPT_DESC, "", "", "", "fzf_tmux_command_cb", "")
+    elif kitty:
+        weechat.hook_command(SCRIPT_NAME, SCRIPT_DESC, "", "", "", "fzf_kitty_command_cb", "")
+    else:
+        print_error("Error: fzf.py currently only supports being run inside tmux or kitty")
 
 if __name__ == "__main__":
     main()
