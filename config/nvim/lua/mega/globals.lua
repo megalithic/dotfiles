c4ecb3f0d4067ffa8bd705838deeab351381b442
Seth Messer <seth.messer@gmail.com>
2022-03-20 20:08:32 -0500

    chore(globals): adds autoresize;
    thanks akinsho!

--- config/nvim/lua/mega/globals.lua
+++ config/nvim/lua/mega/globals.lua
@@ -823,6 +823,34 @@ function mega.colors()
   return lush(spec)
 end
 
+-----------------------------------------------------------------------------//
+-- Autoresize
+-----------------------------------------------------------------------------//
+-- Auto resize Vim splits to active split to 70% -
+-- https://stackoverflow.com/questions/11634804/vim-auto-resize-focused-window
+function mega.auto_resize()
+  local auto_resize_on = false
+  return function(args)
+    if not auto_resize_on then
+      local factor = args and tonumber(args) or 70
+      local fraction = factor / 10
+      -- NOTE: mutating &winheight/&winwidth are key to how
+      -- this functionality works, the API fn equivalents do
+      -- not work the same way
+      vim.cmd(fmt("let &winheight=&lines * %d / 10 ", fraction))
+      vim.cmd(fmt("let &winwidth=&columns * %d / 10 ", fraction))
+      auto_resize_on = true
+      vim.notify("Auto resize ON")
+    else
+      vim.cmd("let &winheight=30")
+      vim.cmd("let &winwidth=30")
+      vim.cmd("wincmd =")
+      auto_resize_on = false
+      vim.notify("Auto resize OFF")
+    end
+  end
+end
+
 -- [ commands ] ----------------------------------------------------------------
 do
   local command = mega.command
@@ -830,6 +858,7 @@ do
     command! -nargs=1 Rg lua require("telescope.builtin").grep_string({ search = vim.api.nvim_eval('"<args>"') })
   ]])
 
+  command("AutoResize", mega.auto_resize(), { nargs = "?" })
   command("Todo", [[noautocmd silent! grep! 'TODO\|FIXME\|BUG\|HACK' | copen]])
   command(
     "Duplicate",
