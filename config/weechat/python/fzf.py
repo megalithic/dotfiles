d3ec3d0ba9c9b8b54873147d5559c100138d293b
Seth Messer <seth.messer@gmail.com>
2022-04-08 13:32:45 -0500

    wip(espanso): migrate back to 0.7.3 legacy;
    - legacy expands things WAY faster

--- config/weechat/python/fzf.py
+++ config/weechat/python/fzf.py
@@ -1,4 +1,5 @@
 from typing import Iterator, Tuple
+
 import weechat
 
 SCRIPT_NAME = "fzf"
@@ -8,11 +9,14 @@ SCRIPT_LICENSE = "MIT"
 SCRIPT_DESC = "Switch buffer using fzf (currently only works inside tmux and kitty)"
 REPO_URL = "https://github.com/trygveaa/weechat-fzf"
 
+
 def print_log(message: str) -> None:
     weechat.prnt("", weechat.prefix("debug") + message)
 
+
 def print_error(message: str) -> None:
     weechat.prnt("", weechat.prefix("error") + message)
+
 
 def print_warn(message: str) -> None:
     weechat.prnt("", weechat.prefix("warning") + message)
@@ -21,14 +25,14 @@ def print_warn(message: str) -> None:
 def fzf_process_cb(
     data: str, command: str, return_code: int, out: str, err: str
 ) -> int:
-    print_log("Debugging.. data: {}".format(data))
-    print_log("Debugging.. command: {}".format(command))
+    # print_log("Debugging.. data: {}".format(data))
+    # print_log("Debugging.. command: {}".format(command))
 
     if return_code == weechat.WEECHAT_HOOK_PROCESS_ERROR or return_code == 2 or err:
         print_error("Error running fzf (code {}): {}".format(return_code, err))
         return weechat.WEECHAT_RC_OK
     if out != "":
-        print_log("Debugging.. out: {}".format(out))
+        # print_log("Debugging.. out: {}".format(out))
 
         pointer, _ = out.split("\t", 1)
         weechat.buffer_set(pointer, "display", "1")
@@ -46,19 +50,21 @@ def fzf_tmux_command_cb(data: str, buffer: str, args: str) -> int:
     weechat.hook_set(hook, "stdin_close", "")
     return weechat.WEECHAT_RC_OK
 
+
 def fzf_kitty_command_cb(data: str, buffer: str, args: str) -> int:
-        # "kitty @ launch --type=overlay --keep-focus --hold --stdin-source=@last_cmd_output --stdin-add-formatting--stdin-add-line-wrap-markers \""
+    # "kitty @ launch --type=overlay --keep-focus --hold --stdin-source=@last_cmd_output --stdin-add-formatting--stdin-add-line-wrap-markers \""
     cmd = (
         "kitty @ launch --type=overlay --keep-focus --hold "
         "fzf -- --delimiter='\t' --with-nth=3.. "
         "--preview='tail -$LINES {2} 2>/dev/null'"
     )
     hook = weechat.hook_process_hashtable(cmd, {"stdin": "1"}, 0, "fzf_process_cb", "")
-    print_log("Debugging.. hook: {}".format(hook))
+    # print_log("Debugging.. hook: {}".format(hook))
     for buffer_info in buffers():
         weechat.hook_set(hook, "stdin", "\t".join(buffer_info) + "\n")
     weechat.hook_set(hook, "stdin_close", "")
     return weechat.WEECHAT_RC_OK
+
 
 def buffers() -> Iterator[Tuple[str, str, str, str]]:
     logger_filenames = {}
@@ -77,6 +83,7 @@ def buffers() -> Iterator[Tuple[str, str, str, str]]:
         yield (pointer, logger_filenames.get(pointer, ""), str(number), name)
     weechat.infolist_free(buffer_infolist)
 
+
 def main() -> None:
     if not weechat.register(
         SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT_DESC, "", ""
@@ -86,11 +93,18 @@ def main() -> None:
     tmux = weechat.string_eval_expression("${env:TMUX}", {}, {}, {})
     kitty = weechat.string_eval_expression("${env:KITTY_LISTEN_ON}", {}, {}, {})
     if tmux:
-        weechat.hook_command(SCRIPT_NAME, SCRIPT_DESC, "", "", "", "fzf_tmux_command_cb", "")
+        weechat.hook_command(
+            SCRIPT_NAME, SCRIPT_DESC, "", "", "", "fzf_tmux_command_cb", ""
+        )
     elif kitty:
-        weechat.hook_command(SCRIPT_NAME, SCRIPT_DESC, "", "", "", "fzf_kitty_command_cb", "")
+        weechat.hook_command(
+            SCRIPT_NAME, SCRIPT_DESC, "", "", "", "fzf_kitty_command_cb", ""
+        )
     else:
-        print_error("Error: fzf.py currently only supports being run inside tmux or kitty")
+        print_error(
+            "Error: fzf.py currently only supports being run inside tmux or kitty"
+        )
+
 
 if __name__ == "__main__":
     main()
