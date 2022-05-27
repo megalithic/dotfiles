e95d2bd2a99f7c841b0599602390eb84b43682ca
Seth Messer <seth.messer@gmail.com>
2022-05-27 16:00:38 -0500

    wip: cleanup

--- config/zsh/plugins.zsh
+++ config/zsh/plugins.zsh
@@ -8,16 +8,18 @@ function zsh_add_file() {
 }
 
 function zsh_source_plugin () {
-  zsh_add_file "plugins/$1.zsh"
+  zsh_add_file "$PLUGIN_DIR/$1.zsh"
 }
 
 function zsh_add_plugin() {
   PLUGIN_NAME=$(echo $1 | cut -d "/" -f 2)
-  if [ -d "$ZDOTDIR/plugins/$PLUGIN_NAME" ]; then
-    zsh_add_file "plugins/$PLUGIN_NAME/$PLUGIN_NAME.plugin.zsh" || \
-    zsh_add_file "plugins/$PLUGIN_NAME/$PLUGIN_NAME.zsh" || \
-    zsh_add_file "plugins/$PLUGIN_NAME/$2.zsh"
+  if [ -d "$PLUGIN_DIR/$PLUGIN_NAME" ]; then
+    zsh_add_file "$PLUGIN_DIR/$PLUGIN_NAME/$PLUGIN_NAME.plugin.zsh" || \
+    zsh_add_file "$PLUGIN_DIR/$PLUGIN_NAME/$PLUGIN_NAME.zsh" || \
+    zsh_add_file "$PLUGIN_DIR/$PLUGIN_NAME/$2.zsh"
   else
+    echo "1: $1"
+    echo "$PLUGIN_DIR/$PLUGIN_NAME"
     git submodule add "https://github.com/$1" "$PLUGIN_DIR/$PLUGIN_NAME"
   fi
 }
