be8768d70aee20286e76494169b104e836313591
Seth Messer <seth.messer@gmail.com>
2022-05-31 12:56:30 -0500

    chore(telescope): fuzzy = true

--- config/nvim/lua/mega/plugins/telescope.lua
+++ config/nvim/lua/mega/plugins/telescope.lua
@@ -122,6 +122,7 @@ return function(plug)
         find_cmd = "rg",
       },
       fzf = {
+        fuzzy = true, -- false will only do exact matching
         override_generic_sorter = true, -- override the generic sorter
         override_file_sorter = true, -- override the file sorter
         case_mode = "smart_case", -- or "ignore_case" or "respect_case"
