c5a4212fa4bfdf91d2541b8425f4608e5ca8af81
Seth Messer <seth.messer@gmail.com>
2022-06-16 14:33:45 -0500

    fix(lsp): don't override definition handler for now

--- config/nvim/plugin/lsp.lua
+++ config/nvim/plugin/lsp.lua
@@ -508,28 +508,28 @@ local function setup_handlers()
     end
   end
 
-  lsp.handlers["textDocument/definition"] = function(_, result)
-    if result == nil or vim.tbl_isempty(result) then
-      print("Definition not found")
-      return nil
-    end
-    local function jumpto(loc)
-      local split_cmd = vim.uri_from_bufnr(0) == loc.targetUri and "edit" or "vnew"
-      vim.cmd(split_cmd)
-      lsp.util.jump_to_location(loc)
-    end
-    if vim.tbl_islist(result) then
-      jumpto(result[1])
+  -- lsp.handlers["textDocument/definition"] = function(_, result)
+  --   if result == nil or vim.tbl_isempty(result) then
+  --     print("Definition not found")
+  --     return nil
+  --   end
+  --   local function jumpto(loc)
+  --     local split_cmd = vim.uri_from_bufnr(0) == loc.targetUri and "edit" or "vnew"
+  --     vim.cmd(split_cmd)
+  --     lsp.util.jump_to_location(loc)
+  --   end
+  --   if vim.tbl_islist(result) then
+  --     jumpto(result[1])
 
-      if #result > 1 then
-        fn.setqflist(lsp.util.locations_to_items(result))
-        api.nvim_command("copen")
-        api.nvim_command("wincmd p")
-      end
-    else
-      jumpto(result)
-    end
-  end
+  --     if #result > 1 then
+  --       fn.setqflist(lsp.util.locations_to_items(result))
+  --       api.nvim_command("copen")
+  --       api.nvim_command("wincmd p")
+  --     end
+  --   else
+  --     jumpto(result)
+  --   end
+  -- end
 end
 
 -- [ HANDLERS ] ----------------------------------------------------------------
