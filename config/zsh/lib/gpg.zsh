107d9bef8c1881426c145ca526902ab267cbe226
Seth Messer <seth@megalithic.io>
2022-06-28 08:14:02 -0500

    things

--- config/zsh/lib/gpg.zsh
+++ config/zsh/lib/gpg.zsh
@@ -3,3 +3,4 @@ export "SSH_AUTH_SOCK=${HOME}/.gnupg/S.gpg-agent.ssh"
 # gpg-connect-agent updatestartuptty /bye
 unset SSH_AGENT_PID
 export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+gpgconf --launch gpg-agent
