80cb1cbe27393d5fbfa5902ce49224e8b52a60f0
Seth Messer <seth@megalithic.io>
2022-06-28 09:55:29 -0500

    feat(nvim): markdown TS conceal characters

--- config/nvim/after/queries/markdown/highlights.scm
+++ config/nvim/after/queries/markdown/highlights.scm
@@ -21,3 +21,10 @@
 ;     (set! "priority" 2050)
 ; )
 ; (inline_link (link_text) @MDTSLinkText) @MDTSLink
+
+; TODO: https://github.com/akinsho/org-bullets.nvim/blob/main/lua/org-bullets.lua#L167-L188
+; handle these conceals with regex matches instead:
+((list_marker_star) @conceal (#set! conceal "✸  ") (#eq? @conceal "* "))
+((list_marker_plus) @conceal (#set! conceal "✿  ") (#eq? @conceal "+ "))
+((list_marker_minus) @conceal (#set! conceal "•  ") (#eq? @conceal "- "))
+((list_marker_dot) @conceal (#set! conceal "•  ") (#eq? @conceal ". "))
