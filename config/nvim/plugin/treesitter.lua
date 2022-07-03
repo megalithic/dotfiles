39d3cab8f4f14097b1f42af7395f2e98e08e7241
Seth Messer <seth@megalithic.io>
2022-07-02 22:26:19 -0500

    fix(nvim): ts dec. node select fails with <C-CR>

--- config/nvim/plugin/treesitter.lua
+++ config/nvim/plugin/treesitter.lua
@@ -139,7 +139,7 @@ require("nvim-treesitter.configs").setup({
       -- scope_incremental = "grc", -- (grc) increment to the upper scope (as defined in locals.scm)
       init_selection = "<CR>", -- maps in normal mode to init the node/scope selection
       node_incremental = "<CR>", -- increment to the upper named parent
-      node_decremental = "<C-CR>", -- decrement to the previous node
+      node_decremental = "<BS>", -- decrement to the previous node
     },
   },
   textobjects = {
