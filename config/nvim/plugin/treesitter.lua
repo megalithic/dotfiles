9fe160c32325b19f47df95ddbf9d7508c960cf04
Seth Messer <seth@megalithic.io>
2022-07-08 09:30:14 -0500

    fix(ts): don't use regex highlighting for elixir

--- config/nvim/plugin/treesitter.lua
+++ config/nvim/plugin/treesitter.lua
@@ -8,43 +8,6 @@ ft_to_parser.json = "jsonc"
 ft_to_parser.keymap = "devicetree"
 ft_to_parser.zsh = "bash"
 
-mega.treesitter = mega.treesitter or {
-  install_attempted = {},
-}
-
--- When visiting a file with a type we don't have a parser for, ask me if I want to install it.
-function mega.treesitter.ensure_parser_installed()
-  local WAIT_TIME = 6000
-  local ignored_langs = {}
-  local parsers = require("nvim-treesitter.parsers")
-  local lang = parsers.get_buf_lang()
-  local fmt = string.format
-  if
-    parsers.get_parser_configs()[lang]
-    and not parsers.has_parser(lang)
-    and not mega.treesitter.install_attempted[lang]
-  then
-    vim.schedule(function()
-      if vim.tbl_contains(ignored_langs, lang) then return end
-      vim.cmd("TSInstall " .. lang)
-      mega.treesitter.install_attempted[lang] = true
-      vim.notify(fmt("Installing missing Treesitter parser for %s", lang), "info", {
-        title = "Nvim Treesitter",
-        icon = mega.icons.misc.down,
-        timeout = WAIT_TIME,
-      })
-    end)
-  end
-end
-
--- mega.augroup("TSParserCheck", {
---   {
---     event = "FileType",
---     desc = "Treesitter: install missing parsers",
---     command = mega.treesitter.ensure_parser_installed,
---   },
--- })
-
 require("nvim-treesitter.configs").setup({
   auto_install = true,
   ensure_installed = {
@@ -96,7 +59,7 @@ require("nvim-treesitter.configs").setup({
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = { "elixir" },
+    additional_vim_regex_highlighting = {},
     use_languagetree = true,
   },
   indent = { enable = true },
@@ -141,9 +104,9 @@ require("nvim-treesitter.configs").setup({
     },
   },
   textobjects = {
-    lookahead = true,
     select = {
       enable = true,
+      lookahead = true,
       keymaps = {
         ["af"] = "@function.outer",
         ["if"] = "@function.inner",
