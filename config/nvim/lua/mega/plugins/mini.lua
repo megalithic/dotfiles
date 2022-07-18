e623f8d97576daacc33e9f140347dd2530861bb5
Seth Messer <seth@megalithic.io>
2022-07-18 10:27:25 -0500

    fix(nvim): back to hop for f/t things

--- config/nvim/lua/mega/plugins/mini.lua
+++ config/nvim/lua/mega/plugins/mini.lua
@@ -15,32 +15,32 @@ return function()
     -- symbol = "▏",
   })
 
-  require("mini.jump").setup({
-    -- Module mappings. Use `''` (empty string) to disable one.
-    mappings = {
-      forward = "f",
-      backward = "F",
-      forward_till = "t",
-      backward_till = "T",
-      repeat_jump = ";",
-    },
+  -- require("mini.jump").setup({
+  --   -- Module mappings. Use `''` (empty string) to disable one.
+  --   mappings = {
+  --     forward = "f",
+  --     backward = "F",
+  --     forward_till = "t",
+  --     backward_till = "T",
+  --     repeat_jump = ";",
+  --   },
 
-    -- Delay values (in ms) for different functionalities. Set any of them to
-    -- a very big number (like 10^7) to virtually disable.
-    delay = {
-      -- Delay between jump and highlighting all possible jumps
-      highlight = 250,
+  --   -- Delay values (in ms) for different functionalities. Set any of them to
+  --   -- a very big number (like 10^7) to virtually disable.
+  --   delay = {
+  --     -- Delay between jump and highlighting all possible jumps
+  --     highlight = 250,
 
-      -- Delay between jump and automatic stop if idle (no jump is done)
-      idle_stop = 10000000,
-    },
+  --     -- Delay between jump and automatic stop if idle (no jump is done)
+  --     idle_stop = 10000000,
+  --   },
 
-    -- Functions to be executed at certain events
-    -- hooks = {
-    --   before_start = nil, -- Before jump start
-    --   after_jump = function() mega.blink_cursorline() end, -- After jump was actually done
-    -- },
-  })
+  --   -- Functions to be executed at certain events
+  --   -- hooks = {
+  --   --   before_start = nil, -- Before jump start
+  --   --   after_jump = function() mega.blink_cursorline() end, -- After jump was actually done
+  --   -- },
+  -- })
 
   -- require("mini.jump2d").setup({
   --   enable = false,
