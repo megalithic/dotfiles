1a8c463e6b3f06dd7de320ff1533e8acece3fcc1
Seth Messer <seth@megalithic.io>
2022-08-10 16:02:08 -0500

    chore(nvim): adds sign handler back

--- config/nvim/plugin/lsp.lua
+++ config/nvim/plugin/lsp.lua
@@ -303,11 +303,11 @@ local function setup_diagnostics()
     end
   end
 
-  -- local signs_handler = vim.diagnostic.handlers.signs
-  -- vim.diagnostic.handlers.signs = {
-  --   show = max_diagnostic(signs_handler.show),
-  --   hide = function(_, bufnr) signs_handler.hide(ns, bufnr) end,
-  -- }
+  local signs_handler = vim.diagnostic.handlers.signs
+  vim.diagnostic.handlers.signs = {
+    show = max_diagnostic(signs_handler.show),
+    hide = function(_, bufnr) signs_handler.hide(ns, bufnr) end,
+  }
 
   -- local virt_text_handler = vim.diagnostic.handlers.virtual_text
   -- vim.diagnostic.handlers.virtual_text = {
