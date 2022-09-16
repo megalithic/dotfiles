1329241cba69452fdee9196eb5dd4df8f30530ac
Seth Messer <seth@megalithic.io>
2022-09-16 15:44:05 -0500

    wip(packer)

--- config/nvim/lua/mega/plugins/packer.lua
+++ config/nvim/lua/mega/plugins/packer.lua
@@ -1,12 +1,79 @@
 local fmt = string.format
 
-local _use, use_local, bootstrap_packer, packer_notify, conf = unpack(require("mega.plugins.utils"))
-local mega = require("mega.globals")
+-- @HT tiagovla/.dotfiles for this packer boilerplate
 
+local fn = vim.fn
+local fmt = string.format
+local mega = _G.mega or require("mega.globals")
+
+local PACKER_INSTALL_PATH = fmt("%s/site/pack/packer/%s/packer.nvim", fn.stdpath("data"), "start")
 local PACKER_COMPILED_PATH = fmt("%s/packer/packer_compiled.lua", vim.fn.stdpath("cache"))
 local PACKER_SNAPSHOTS_PATH = fmt("%s/packer/snapshots/", vim.fn.stdpath("cache"))
 
-local bootstrapped = bootstrap_packer("start", PACKER_COMPILED_PATH)
+-- ---A thin wrapper around vim.notify to add packer details to the message
+-- ---@param msg string
+local function packer_notify(msg, level) vim.notify(msg, level, { title = "Packer" }) end
+
+local function bootstrap_packer()
+  if fn.empty(fn.glob(PACKER_INSTALL_PATH)) > 0 then
+    packer_notify("Downloading packer.nvim...")
+    packer_notify(
+      fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", PACKER_INSTALL_PATH })
+    )
+    vim.fn.delete(PACKER_COMPILED_PATH)
+    vim.cmd.packadd({ "packer.nvim", bang = true })
+    -- require("packer").sync()
+    return true
+    -- else
+    --   vim.cmd.packadd({ "packer.nvim", bang = true })
+  end
+  return false
+end
+
+-- function _G.packer_upgrade()
+--   vim.fn.delete(PACKER_INSTALL_PATH, "rf")
+--   bootstrap_packer("start", PACKER_COMPILED_PATH)
+-- end
+--
+-- vim.cmd.command({ "PackerUpgrade", ":call v:lua.packer_upgrade()", bang = true })
+
+local function load(path)
+  require("mega.plugins." .. path)
+  local ok_conf, res = pcall(require, "mega.plugins." .. path)
+  if ok_conf then
+    return res
+  else
+    packer_notify(fmt("Could not load %s", path))
+    return {}
+  end
+end
+
+local function use(spec)
+  if spec.ext then
+    local config_ext = load(spec.ext)
+    spec = vim.tbl_deep_extend("force", spec, config_ext)
+    spec.ext = nil
+  end
+
+  require("packer").use(spec)
+end
+
+-- hard coded local plugins path
+local function use_local(spec)
+  local sp = vim.split(spec[1], "/")
+  local repo_path = vim.fn.expand(fmt("%s/%s", vim.env.CODE, sp[#sp]), nil, nil)
+  if vim.fn.isdirectory(repo_path) == 1 then
+    spec[1] = repo_path
+  else
+    packer_notify(fmt("Could not load local %s, using online instead.", repo_path))
+  end
+
+  use(spec)
+end
+
+local function conf(name) return require(fmt("mega.plugins.%s", name)) end
+
+local bootstrapped = bootstrap_packer()
 
 vim.cmd.packadd({ "cfilter", bang = true })
 
@@ -645,9 +712,9 @@ require("packer").startup({
   -- log = { level = "info" },
   config = {
     display = {
-      open_cmd = "silent topleft 65vnew",
-      -- open_fn = function() return require("packer.util").float({ border = "single" }) end,
-      -- prompt_border = mega.get_border(),
+      -- open_cmd = "silent topleft 65vnew",
+      open_fn = function() return require("packer.util").float({ border = mega.get_border() }) end,
+      prompt_border = mega.get_border(),
     },
     non_interactive = vim.env.PACKER_NON_INTERACTIVE or false,
     compile_path = PACKER_COMPILED_PATH,
--- config/nvim/lua/mega/plugins/utils.lua
+++ config/nvim/lua/mega/plugins/utils.lua
@@ -1,72 +1,115 @@
--- @HT tiagovla/.dotfiles for this packer boilerplate
+-- @HTfunction  tiagovla/.dotfiles for this packer boilerplate
 
 local fn = vim.fn
 local fmt = string.format
+local M = { local_plugins = {} }
+local PACKER_INSTALL_PATH = fmt("%s/site/pack/packer/%s/packer.nvim", fn.stdpath("data"), "opt")
 
 -- ---A thin wrapper around vim.notify to add packer details to the message
 -- ---@param msg string
-local function packer_notify(msg, level) vim.notify(msg, level, { title = "Packer" }) end
+function M.packer_notify(msg, level) vim.notify(msg, level, { title = "Packer" }) end
 
-local function bootstrap_packer(rtp_method, compiled_path)
-  rtp_method = rtp_method or "start"
-  local PACKER_INSTALL_PATH = fmt("%s/site/pack/packer/%s/packer.nvim", fn.stdpath("data"), rtp_method)
+-- function M.conf(name) return require(fmt("mega.plugins.%s", name)) end
 
+local function reload()
+  mega.invalidate("mega.plugins", true)
+  require("packer").compile()
+end
+
+local function setup_autocmds()
+  mega.augroup("PackerSetupInit", {
+    {
+      event = { "BufWritePost" },
+      pattern = { "*/mega/plugins/*.lua" },
+      desc = "Packer setup and reload",
+      command = reload,
+    },
+    {
+      event = { "User" },
+      pattern = { "VimrcReloaded" },
+      desc = "Packer setup and reload",
+      command = reload,
+    },
+    {
+      event = { "User" },
+      pattern = { "PackerCompileDone" },
+      command = function() M.packer_notify("Compilation finished", "info") end,
+    },
+  })
+end
+
+function M.bootstrap()
+  local fn = vim.fn
   if fn.empty(fn.glob(PACKER_INSTALL_PATH)) > 0 then
-    packer_notify("Downloading packer.nvim...")
-    packer_notify(
-      fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", PACKER_INSTALL_PATH })
-    )
-    vim.fn.delete(compiled_path)
-    vim.cmd.packadd({ "packer.nvim", bang = true })
-    -- require("packer").sync()
-    return true
-    -- else
-    --   vim.cmd.packadd({ "packer.nvim", bang = true })
+    fn.system({ "git", "clone", "https://github.com/wbthomason/packer.nvim", PACKER_INSTALL_PATH })
+    vim.cmd("packadd packer.nvim")
   end
-  return false
+  vim.cmd([[packadd packer.nvim]])
+  setup_autocmds()
 end
 
--- function _G.packer_upgrade()
---   vim.fn.delete(PACKER_INSTALL_PATH, "rf")
---   bootstrap_packer("start", PACKER_COMPILED_PATH)
--- end
---
--- vim.cmd.command({ "PackerUpgrade", ":call v:lua.packer_upgrade()", bang = true })
+function M.get_name(pkg)
+  local parts = vim.split(pkg, "/")
+  return parts[#parts], parts[1]
+end
 
-local function load(path)
-  require("mega.plugins." .. path)
-  local ok_conf, res = pcall(require, "mega.plugins." .. path)
-  if ok_conf then
-    return res
+function M.has_local(name) return vim.loop.fs_stat(vim.fn.expand(fmt("%s/%s", vim.env.CODE, name))) ~= nil end
+
+-- This method replaces any plugins with the local clone under vim.env.CODE
+function M.process_local_plugins(spec)
+  if type(spec) == "string" then
+    local name, owner = M.get_name(spec)
+    local local_pkg = fmt("%s/%s", vim.env.CODE, name)
+
+    if M.local_plugins[name] or M.local_plugins[owner] or M.local_plugins[owner .. "/" .. name] then
+      if M.has_local(name) then
+        return local_pkg
+      else
+        M.packer_notify("Local package " .. name .. " not found", "ERROR")
+      end
+    end
+    return spec
   else
-    packer_notify(fmt("Could not load %s", path))
-    return {}
+    for i, s in ipairs(spec) do
+      spec[i] = M.process_local_plugins(s)
+    end
   end
+  if spec.requires then spec.requires = M.process_local_plugins(spec.requires) end
+  return spec
 end
 
-local function use(spec)
+function M.process_ext_configs(spec)
   if spec.ext then
     local config_ext = load(spec.ext)
     spec = vim.tbl_deep_extend("force", spec, config_ext)
     spec.ext = nil
   end
-
-  require("packer").use(spec)
+  return spec
 end
 
--- hard coded local plugins path
-local function use_local(spec)
-  local sp = vim.split(spec[1], "/")
-  local repo_path = vim.fn.expand(fmt("%s/%s", vim.env.CODE, sp[#sp]), nil, nil)
-  if vim.fn.isdirectory(repo_path) == 1 then
-    spec[1] = repo_path
-  else
-    packer_notify(fmt("Could not load local %s, using online instead.", repo_path))
+function M.wrap(use)
+  return function(spec)
+    spec = M.process_local_plugins(spec)
+    spec = M.process_ext_configs(spec)
+
+    use(spec)
   end
-
-  use(spec)
 end
 
-local function conf(name) return require(fmt("mega.plugins.%s", name)) end
+function M.setup(config, plugins_fn)
+  -- HACK: see https://github.com/wbthomason/packer.nvim/issues/180
+  vim.fn.setenv("MACOSX_DEPLOYMENT_TARGET", "10.15")
 
-return { use, use_local, bootstrap_packer, packer_notify, conf }
+  M.bootstrap()
+  local packer = require("packer")
+  packer.init(config)
+  M.local_plugins = config.local_plugins or {}
+  return packer.startup({
+    function(use)
+      use = M.wrap(use)
+      plugins_fn(use)
+    end,
+  })
+end
+
+return M
