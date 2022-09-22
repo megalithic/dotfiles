85fd76fc755b965273a1a05e6859701cc876bbc9
Seth Messer <seth@megalithic.io>
2022-09-22 09:31:39 -0500

    chore(nvim/zsh): adds multi-cursor; cdr not in vim

--- config/nvim/lua/mega/plugins/init.lua
+++ config/nvim/lua/mega/plugins/init.lua
@@ -619,6 +619,19 @@ local function plugins(use)
     end,
   })
   use({
+    "mg979/vim-visual-multi",
+    config = function()
+      vim.g.VM_highlight_matches = "underline"
+      vim.g.VM_theme = "codedark"
+      vim.g.VM_maps = {
+        ["Find Under"] = "<C-e>",
+        ["Find Subword Under"] = "<C-e>",
+        ["Select Cursor Down"] = "\\j",
+        ["Select Cursor Up"] = "\\k",
+      }
+    end,
+  })
+  use({
     "windwp/nvim-autopairs",
     after = "nvim-cmp",
     requires = "nvim-cmp",
