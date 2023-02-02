690892042e60fdb3b4e2d755af13f5b5b03bdab2
Seth Messer <seth@megalithic.io>
2023-02-01 19:25:29 -0600

    chore: address linting error

--- asdf/python.sh
+++ asdf/python.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/zsh
+# shellcheck shell=bash
 
-[[ -f "$XDG_CONFIG_HOME/zsh/lib/helpers.zsh" ]] && source "$XDG_CONFIG_HOME/zsh/lib/helpers.zsh"
+[[ -f "$HOME/.dotfiles/config/zsh/lib/helpers.zsh" ]] && source "$HOME/.dotfiles/config/zsh/lib/helpers.zsh"
 
 set -euo pipefail
 
@@ -14,14 +15,22 @@ __invocation="$(printf %q "$__file")$( (($#)) && printf ' %q' "$@" || true)"
 
 do_install() {
   python3 -m pip install --upgrade pip
-  cat "$HOME"/.default-python-packages | xargs python3 -m pip install --upgrade --user
+  python3 -m pip install --upgrade --user -r "$HOME/.default-python-packages"
+  # cat "$HOME/.default-python-packages" | xargs python3 -m pip install --upgrade --user
   python3.10 -m pip install --upgrade pip
-  cat "$HOME"/.default-python-packages | xargs python3.10 -m pip install --upgrade --user
+  python3.10 -m pip install --upgrade --user -r "$HOME/.default-python-packages"
+  # cat "$HOME/.default-python-packages" | xargs python3.10 -m pip install --upgrade --user
   python3.11 -m pip install --upgrade pip
-  cat "$HOME"/.default-python-packages | xargs python3.11 -m pip install --upgrade --user
-  cat "$HOME"/.default-python-packages | xargs pip3.11 install --upgrade --user
-  cat "$HOME"/.default-python-packages | xargs pip3 install --upgrade --user
-  sudo cat "$HOME"/.default-python-packages | xargs /Library/Frameworks/Python.framework/Versions/3.11/bin/pip3 install --upgrade --user
+  python3.11 -m pip install --upgrade --user -r "$HOME/.default-python-packages"
+  # cat "$HOME/.default-python-packages" | xargs python3.11 -m pip install --upgrade --user
+  pip3.11 -m pip install --upgrade --user -r "$HOME/.default-python-packages"
+  # cat "$HOME/.default-python-packages" | xargs pip3.11 install --upgrade --user
+  pip3 -m pip install --upgrade --user -r "$HOME/.default-python-packages"
+  # cat "$HOME/.default-python-packages" | xargs pip3 install --upgrade --user
+  sudo /Library/Frameworks/Python.framework/Versions/3.11/bin/pip3 -m pip install --upgrade --user -r "$HOME/.default-python-packages"
+  # sudo cat "$HOME/.default-python-packages" | xargs /Library/Frameworks/Python.framework/Versions/3.11/bin/pip3 install --upgrade --user
+
+  asdf reshim python
 }
 
 do_install || exit 1
