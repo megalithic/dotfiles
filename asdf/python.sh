9b954c7887b250a88851da0279e554dd5f4988e7
Seth Messer <seth@megalithic.io>
2023-02-02 16:34:26 -0600

    fix: python (again); fwd tui; add postbird

--- asdf/python.sh
+++ asdf/python.sh
@@ -15,20 +15,21 @@ __invocation="$(printf %q "$__file")$( (($#)) && printf ' %q' "$@" || true)"
 
 do_install() {
   python3 -m pip install --upgrade pip
-  python3 -m pip install --upgrade --user -r "$HOME/.default-python-packages"
-  # cat "$HOME/.default-python-packages" | xargs python3 -m pip install --upgrade --user
+  python3 -m pip install --upgrade -r "$HOME/.default-python-packages"
+  # cat "$HOME/.default-python-packages" | xargs python3 -m pip install --upgrade
   python3.10 -m pip install --upgrade pip
-  python3.10 -m pip install --upgrade --user -r "$HOME/.default-python-packages"
-  # cat "$HOME/.default-python-packages" | xargs python3.10 -m pip install --upgrade --user
+  python3.10 -m pip install --upgrade -r "$HOME/.default-python-packages"
+  # cat "$HOME/.default-python-packages" | xargs python3.10 -m pip install --upgrade
   python3.11 -m pip install --upgrade pip
-  python3.11 -m pip install --upgrade --user -r "$HOME/.default-python-packages"
-  # cat "$HOME/.default-python-packages" | xargs python3.11 -m pip install --upgrade --user
-  pip3.11 -m pip install --upgrade --user -r "$HOME/.default-python-packages"
-  # cat "$HOME/.default-python-packages" | xargs pip3.11 install --upgrade --user
-  pip3 -m pip install --upgrade --user -r "$HOME/.default-python-packages"
-  # cat "$HOME/.default-python-packages" | xargs pip3 install --upgrade --user
-  sudo /Library/Frameworks/Python.framework/Versions/3.11/bin/pip3 -m pip install --upgrade --user -r "$HOME/.default-python-packages"
-  # sudo cat "$HOME/.default-python-packages" | xargs /Library/Frameworks/Python.framework/Versions/3.11/bin/pip3 install --upgrade --user
+  python3.11 -m pip install --upgrade -r "$HOME/.default-python-packages"
+  # cat "$HOME/.default-python-packages" | xargs python3.11 -m pip install --upgrade
+  pip3.11 -m pip install --upgrade  -r "$HOME/.default-python-packages"
+  # cat "$HOME/.default-python-packages" | xargs pip3.11 install --upgrade
+  pip3 -m pip install --upgrade  -r "$HOME/.default-python-packages"
+  # cat "$HOME/.default-python-packages" | xargs pip3 install --upgrade
+  # sudo /Library/Frameworks/Python.framework/Versions/3.11/bin/pip3 -m pip install --upgrade  -r "$HOME/.default-python-packages"
+  /usr/local/opt/python@3.11/bin/python3.11 -m pip install --upgrade  -r "$HOME/.default-python-packages"
+  # sudo cat "$HOME/.default-python-packages" | xargs /Library/Frameworks/Python.framework/Versions/3.11/bin/pip3 install --upgrade
 
   asdf reshim python
 }
