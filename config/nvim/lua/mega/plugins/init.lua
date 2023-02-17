8f62ae4f0e522a7639e4d2f2db91077fbfc4d95f
Seth Messer <seth@megalithic.io>
2023-02-17 00:28:44 -0600

    chore: cleanup

--- config/nvim/lua/mega/plugins/init.lua
+++ config/nvim/lua/mega/plugins/init.lua
@@ -140,7 +140,6 @@ return {
       filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
       show_trailing_blankline_indent = false,
       show_current_context = false,
-      show_current_context_start = true,
     },
   },
 
@@ -161,6 +160,17 @@ return {
       })
       require("mini.indentscope").setup(opts)
     end,
+  },
+
+  {
+    "echasnovski/mini.comment",
+    event = "VeryLazy",
+    opts = {
+      hooks = {
+        pre = function() require("ts_context_commentstring.internal").update_commentstring({}) end,
+      },
+    },
+    config = function(_, opts) require("mini.comment").setup(opts) end,
   },
   -- {
   --   "lukas-reineke/indent-blankline.nvim",
@@ -678,39 +688,39 @@ return {
       -- _G.mega.nnoremap("<leader>db", "<cmd>DBUIToggle<CR>", "dadbod: toggle")
     end,
   },
-  {
-    "numToStr/Comment.nvim",
-    event = "BufRead",
-    config = function()
-      require("Comment").setup({
-
-        ignore = "^$", -- ignores empty lines
-        --@param ctx CommentCtx
-        pre_hook = function(ctx)
-          -- Only calculate commentstring for tsx filetypes
-          if vim.bo.filetype == "typescriptreact" then
-            local U = require("Comment.utils")
-
-            -- Determine whether to use linewise or blockwise commentstring
-            local type = ctx.ctype == U.ctype.line and "__default" or "__multiline"
-
-            -- Determine the location where to calculate commentstring from
-            local location = nil
-            if ctx.ctype == U.ctype.block then
-              location = require("ts_context_commentstring.utils").get_cursor_location()
-            elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-              location = require("ts_context_commentstring.utils").get_visual_start_location()
-            end
-
-            return require("ts_context_commentstring.internal").calculate_commentstring({
-              key = type,
-              location = location,
-            })
-          end
-        end,
-      })
-    end,
-  },
+  -- {
+  --   "numToStr/Comment.nvim",
+  --   event = "BufRead",
+  --   config = function()
+  --     require("Comment").setup({
+  --
+  --       ignore = "^$", -- ignores empty lines
+  --       --@param ctx CommentCtx
+  --       pre_hook = function(ctx)
+  --         -- Only calculate commentstring for tsx filetypes
+  --         if vim.bo.filetype == "typescriptreact" then
+  --           local U = require("Comment.utils")
+  --
+  --           -- Determine whether to use linewise or blockwise commentstring
+  --           local type = ctx.ctype == U.ctype.line and "__default" or "__multiline"
+  --
+  --           -- Determine the location where to calculate commentstring from
+  --           local location = nil
+  --           if ctx.ctype == U.ctype.block then
+  --             location = require("ts_context_commentstring.utils").get_cursor_location()
+  --           elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+  --             location = require("ts_context_commentstring.utils").get_visual_start_location()
+  --           end
+  --
+  --           return require("ts_context_commentstring.internal").calculate_commentstring({
+  --             key = type,
+  --             location = location,
+  --           })
+  --         end
+  --       end,
+  --     })
+  --   end,
+  -- },
   {
     "andymass/vim-matchup",
     event = "BufReadPre",
