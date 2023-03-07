5b279731aa26e05a4874590ccf9eceb070ea6d2b
Seth Messer <seth@megalithic.io>
2023-03-07 08:15:15 -0600

    wip(zsh): docker things

---
+++ config/zsh/lib/docker.zsh
@@ -0,0 +1,9 @@
+#!/usr/bin/env zsh
+
+# REF:
+# https://stackoverflow.com/a/34887761
+# https://pilsniak.com/how-to-install-docker-on-mac-os-using-brew
+
+# docker-machine start # Start virtual machine for docker
+# docker-machine env  # It's helps to get environment variables
+# eval "$(docker-machine env default)" # Set environment variables
