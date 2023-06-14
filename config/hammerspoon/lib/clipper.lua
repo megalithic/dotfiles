2aa2fb95714cf0507fbb7d8b833d65f67d377cca
Seth Messer <seth@megalithic.io>
2023-06-14 17:08:22 -0400

    fix(hs): imgur client id parsed

--- config/hammerspoon/lib/clipper.lua
+++ config/hammerspoon/lib/clipper.lua
@@ -5,7 +5,8 @@ obj.name = "clipper"
 obj.debug = false
 obj.clip_watcher = {}
 obj.clip_data = {}
-obj.tempfile = "/tmp/tmp.png"
+obj.temp_image = "/tmp/tmp.png"
+obj.temp_ocr_image = "/tmp/ocr_tmp.png"
 
 local dbg = function(str, ...)
   str = string.format(":: [%s] %s", obj.name, str)
@@ -16,18 +17,25 @@ function obj.send_to_imgur(image, open_image_url)
   image = image or hs.pasteboard.readImage()
 
   if image then
-    image:saveToFile(obj.tempfile)
-    local b64 = hs.execute("base64 -i " .. obj.tempfile)
-    local client_id = hs.execute("zsh -ci 'echo $IMGUR_CLIENT_ID'")
+    -- clear previous image url
+    hs.pasteboard.clearContents("imageURL")
+
+    image:saveToFile(obj.temp_image)
+    local b64 = hs.execute("base64 -i " .. obj.temp_image)
+
+    local client_id, success, type, rc = hs.execute("zsh -ci 'echo $IMGUR_CLIENT_ID'")
+    dbg("client_id: %s/%s/%s/%s", client_id:gsub("\n", ""), success, type, rc)
 
     if b64 ~= nil then
       b64 = hs.http.encodeForQuery(string.gsub(b64, "\n", ""))
 
       local req_url = "https://api.imgur.com/3/upload.json"
-      local req_headers = { Authorization = fmt("Client-ID %s", client_id) }
+      local req_headers = { Authorization = fmt("Client-ID %s", client_id:gsub("\n", "")) }
       local req_payload = "type='base64'&image=" .. b64
 
-      hs.http.asyncPost(req_url, req_payload, req_headers, function(status, body, _headers)
+      dbg("request: %s/%s", req_url, I(req_headers))
+
+      hs.http.asyncPost(req_url, req_payload, req_headers, function(status, body, headers)
         if status == 200 then
           local response = hs.json.decode(body)
           local imageURL = response.data.link
@@ -35,8 +43,45 @@ function obj.send_to_imgur(image, open_image_url)
           hs.pasteboard.setContents(image, "image")
 
           if open_image_url then hs.urlevent.openURLWithBundle(imageURL, hs.urlevent.getDefaultHandler("http")) end
+          hs.execute("rm " .. obj.temp_image)
+        else
+          error(fmt("status: %s, body: %s, headers: %s", status, body, I(headers)))
         end
       end)
+    end
+  end
+end
+
+-- REF:
+-- Slightly modifed version of:
+-- https://github.com/dbalatero/dotfiles/blob/master/hammerspoon/ocr-paste.lua
+--
+function obj.paste_ocr_text(image)
+  image = image or hs.pasteboard.readImage()
+
+  if image then
+    -- local imagePath = "/tmp/ocr_image.png"
+    local imagePath = obj.temp_ocr_image
+    local outputPath = "/tmp/ocr_output"
+    image:saveToFile(imagePath)
+
+    local output, success, type, rc =
+      hs.execute(fmt("zsh -ic '/opt/homebrew/bin/tesseract -l eng %s %s'", imagePath, outputPath))
+
+    if success then
+      -- Read in OCR result
+      local file = io.open(fmt("%s.txt", outputPath), "r")
+      local content = file:read("*all")
+      dbg("ocrText: %s", content)
+      file:close()
+
+      -- Store OCR content in the pasteboard
+      hs.pasteboard.setContents(content, "ocrText")
+
+      -- clean up
+      hs.execute(fmt("rm %s %s.txt", imagePath, outputPath))
+    else
+      dbg("ocrText [failed]: %s/%s/%s/%s", output, success, type, rc)
     end
   end
 end
@@ -56,6 +101,13 @@ function obj:init(opts)
         local imageURL = hs.pasteboard.getContents("imageURL")
         hs.eventtap.keyStrokes(imageURL)
         dbg("imageURL: %s", imageURL)
+      end)
+
+      hs.hotkey.bind({ "cmd", "shift" }, "p", function()
+        obj.paste_ocr_text(obj.clip_data)
+        local ocr_text = hs.pasteboard.getContents("ocrText")
+        hs.eventtap.keyStrokes(ocr_text)
+        dbg("ocrText: %s", ocr_text)
       end)
     end
   end)
