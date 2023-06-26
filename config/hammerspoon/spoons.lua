bbb457c2c4f22bca1292e6a9fcd874e838c81ae4
Seth Messer <seth@megalithic.io>
2023-06-26 17:08:13 -0400

    chore(hs): cleanup spoons file

--- config/hammerspoon/spoons.lua
+++ config/hammerspoon/spoons.lua
@@ -11,57 +11,26 @@ function obj:init(opts)
   hs.loadSpoon("SpoonInstall")
   spoon.SpoonInstall.use_syncinstall = true
   Install = spoon.SpoonInstall
-
-  -- local config = C
-
-  -- Install:andUse("Seal", {
-  --   -- NOTE: see bindings module for hotkey binding
-  --   start = false,
-  --   fn = function(s)
-  --     s:loadPlugins({ "apps", "calc", "screencapture", "useractions", "urlformats", "safari_bookmarks" })
-  --     s.plugins.safari_bookmarks.always_open_with_safari = false
-  --     s.plugins.useractions.actions = {
-  --       ["Hammerspoon docs webpage"] = {
-  --         url = "http://hammerspoon.org/docs/",
-  --         icon = hs.image.imageFromName(hs.image.systemImageNames.ApplicationIcon),
-  --       },
-  --       ["github"] = { url = "https://github.com/search?q=${query}", keyword = "!gh", icon = "favicon" },
-  --       ["hexdocs"] = { url = "https://hexdocs.pm/${query}", keyword = "!hd", icon = "favicon" },
-  --       ["hex"] = {
-  --         url = "https://hex.pm/packages?search=${query}&sort=recent_downloads",
-  --         keyword = "!hex",
-  --         icon = "favicon",
-  --       },
-  --       ["devdocs"] = { url = "https://devdocs.io/?q=%{query}", keyword = "!dev", icon = "favicon" },
-  --       ["youtube"] = {
-  --         url = "https://www.youtube.com/results?search_query=${query}&page={startPage?}",
-  --         keyword = "!yt",
-  --         icon = "favicon",
-  --       }
-  --     }
-  --     s:refreshAllCommands()
-  --   end,
-  -- })
   Install:andUse("EmmyLua")
   Install:andUse("URLDispatcher", {
     -- TODO: https://github.com/hthuong09/dots/blob/master/.hammerspoon/HandleURLDispatch.lua
     start = true,
-    loglevel = "debug",
+    loglevel = "error",
     config = {
       default_handler = "com.brave.Browser.dev", --hs.application.get(require("hs.settings").get(CONFIG_KEY).preferred.browser),
       url_patterns = {
         { "https?://slack.com/openid/*", "com.brave.Browser.dev" },
+        { "https?://meet.google.com", "com.brave.Browser.dev.app.kjgfgldnnfoeklkmfkjfagphfepbbdan" },
+        { "https?://figma.com", "com.figma.Desktop" },
+        { "https?://open.spotify.com", "com.spotify.client" },
+        { "spotify:", "com.spotify.client" },
+        { "https?://github.com", "com.brave.Browser.dev" },
         -- { "https?://github.com/[mM]iroapp.*", "com.google.Chrome" },
         -- { "https?://[mM]iro.*", "com.google.Chrome" },
         -- { "https?://dev.*.com", "com.google.Chrome" },
         -- { "https?://localhost:*", "com.google.Chrome" },
         -- { "https?://.*devrtb.com", "com.google.Chrome" },
         -- { "https?://www.notion.so", "com.spotify.client" },
-        { "https?://meet.google.com", "com.brave.Browser.dev.app.kjgfgldnnfoeklkmfkjfagphfepbbdan" },
-        { "https?://figma.com", "com.figma.Desktop" },
-        { "https?://open.spotify.com", "com.spotify.client" },
-        { "spotify:", "com.spotify.client" },
-        { "https?://github.com", "com.brave.Browser.dev" },
         -- { "https?://accounts.bellhop.test", "com.apple.Safari" },
         -- { "https?://admin.bellhop.test", "com.apple.Safari" },
       },
