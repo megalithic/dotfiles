c326ed6912e478dfc08876e43baf41524846314a
Seth Messer <seth@megalithic.io>
2024-07-06 16:28:37 -0400

    fix

--- config/nvim/lua/mega/servers.lua
+++ config/nvim/lua/mega/servers.lua
@@ -355,7 +355,7 @@ M.list = function()
         local build_bin = fmt("next_ls_%s_%s", os_name, current_arch)
 
         if use_homebrew then return { "nextls", "--stdio" } end
-        P({ fmt("%s/lsp/next-ls/burrito_out/%s", vim.env.XDG_DATA_HOME, build_bin), "--stdio" })
+        -- P({ fmt("%s/lsp/next-ls/burrito_out/%s", vim.env.XDG_DATA_HOME, build_bin), "--stdio" })
         -- return { fmt("%s/lsp/next-ls/burrito_out/%s", vim.env.XDG_DATA_HOME, build_bin), "--stdio" }
         return { fmt("%s/lsp/bin/nextls", vim.env.XDG_DATA_HOME), "--stdio" }
       end
