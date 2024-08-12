b19e3e5941c131f86a5caf30e5cfa3356a014f0e
Seth Messer <seth@megalithic.io>
2024-08-12 10:01:27 -0400

    chore: comments

--- config/hs/init.lua
+++ config/hs/init.lua
@@ -1,15 +1,18 @@
 G = {} -- persist from garbage collector
 
 ---Try to require the module, and do not error when one of them cannot be
----loaded, but do notify if there was an error.
+---loaded, but go ahead and tell me if something is amiss.
 ---@param mod string module to load
+--- @diagnostic disable-next-line: lowercase-global
 function req(mod)
   local ok, M = pcall(require, mod)
   G[mod:sub(5)] = M
+
   if not ok then
     hs.alert.show(M, 5)
     print(M)
   else
+    -- always return the module.. we end up immediately invoking it, usually.
     return M
   end
 end
@@ -17,6 +20,8 @@ end
 req("preflight")
 req("config")
 req("bindings")
+-- listing of *.watcher based modules; the core of the automation that takes place.
+-- NOTE: `app` contains the app layout and app context logic.
 req("watchers"):start({ "bluetooth", "usb", "dock", "app", "url" })
 req("ptt")
 req("quitter")
