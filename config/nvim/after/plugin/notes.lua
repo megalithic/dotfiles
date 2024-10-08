b5d22393fe9c124eeb050943703114d0f7dcf4fb
Seth Messer <seth@megalithic.io>
2024-10-08 16:04:47 -0400

    chore(nvim/notes): cleans up format notes fn

--- config/nvim/after/plugin/notes.lua
+++ config/nvim/after/plugin/notes.lua
@@ -1,20 +1,11 @@
 if not mega then return end
 
+local U = require("mega.utils")
 local M = {}
 
-vim.api.nvim_create_user_command("FormatNotes", function()
-  local U = require("mega.utils")
-
-  local function extract_tasks(lines)
-    local tasks = {}
-
-    for i, line in ipairs(lines) do
-      local prefix, box, status = line:match("^(%s*[-*] )(%[(.)%])")
-      if prefix ~= nil and box ~= nil and status ~= nil then table.insert(tasks, { index = i, text = line, status = status }) end
-    end
-
-    return tasks, tasks[1].index
-  end
+function M.format_notes(bufnr, lines)
+  bufnr = bufnr or vim.api.nvim_get_current_buf()
+  lines = lines or vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
 
   local function get_priority_from_status(status)
     if status == "." then
@@ -30,34 +21,49 @@ vim.api.nvim_create_user_command("FormatNotes", function()
     end
   end
 
-  local function sort_tasks_by_task_status(tasks)
-    table.sort(tasks, function(a, b) return get_priority_from_status(a.status) < get_priority_from_status(b.status) end)
+  local function extract(lines)
+    local tasks = {}
+
+    for i, line in ipairs(lines) do
+      local prefix, box, status, task_text = line:match("^(%s*[-*] )(%[(.)%])%s(.*)")
+      if prefix ~= nil and box ~= nil and status ~= nil then table.insert(tasks, { index = i, line = line, status = status, text = task_text }) end
+    end
+
+    return tasks, tasks[1].index
   end
 
-  local function replace_tasks(bufnr, sorted_tasks, starting_task_line, _lines)
+  local function sort(tasks)
+    table.sort(tasks, function(a, b)
+      if a.status == b.status then
+        return a.text < b.text -- Sort alphabetically if status are the same
+      else
+        return get_priority_from_status(a.status) < get_priority_from_status(b.status) -- Sort by status
+      end
+    end)
+  end
+
+  local function replace(bufnr, sorted_tasks, starting_task_line, _lines)
     local sorted_tasks_texts = {}
 
     for _, task in ipairs(sorted_tasks) do
-      table.insert(sorted_tasks_texts, task.text)
+      table.insert(sorted_tasks_texts, task.line)
     end
 
     vim.api.nvim_buf_set_lines(bufnr, starting_task_line - 1, starting_task_line + U.tlen(sorted_tasks_texts), false, sorted_tasks_texts)
   end
 
-  local function sort_and_replace_tasks_in_buffer()
-    local bufnr = vim.api.nvim_get_current_buf()
-    local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+  -- extract tasks and the first line the tasks start on
+  local tasks, starting_task_line = extract(lines)
 
-    local tasks, starting_task_line = extract_tasks(lines)
-    local originally_extracted_tasks = U.tcopy(tasks)
+  -- used for comparison later
+  local originally_extracted_tasks = U.tcopy(tasks)
 
-    sort_tasks_by_task_status(tasks)
+  sort(tasks)
 
-    -- verifies our mutated-tasks table (sorted) is different from the tasks before we sorted to know if we ought to replace them. This prevents needless buffer re-writes.
-    if not U.deep_equals(originally_extracted_tasks, tasks) then replace_tasks(bufnr, tasks, starting_task_line, lines) end
-  end
+  -- verifies our mutated-tasks table (sorted) is different from the tasks before we sorted to know if we ought to replace them. This prevents needless buffer re-writes.
+  if not U.deep_equals(originally_extracted_tasks, tasks) then replace(bufnr, tasks, starting_task_line, lines) end
+end
 
-  sort_and_replace_tasks_in_buffer()
-end, {})
+vim.api.nvim_create_user_command("FormatNotes", M.format_notes, {})
 
 return M
