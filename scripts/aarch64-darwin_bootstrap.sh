8a154f624b3538a3b6ed12e581793176b2177409
Seth Messer <seth@megalithic.io>
2026-03-04 17:05:02 -0500

    more misc

--- scripts/aarch64-darwin_bootstrap.sh
+++ scripts/aarch64-darwin_bootstrap.sh
@@ -33,6 +33,8 @@ done 2>/dev/null &
 # sudo rm -rf /Library/Developer/CommandLineTools
 
 # verify commandlinetools not installed:
+
+echo "░ :: -> Checking for Xcode CommandLineTools.."
 if ! xcode-select -p &>/dev/null; then
   # echo "Command Line Tools for Xcode not found. Installing from softwareupdate…"
   # This temporary file prompts the 'softwareupdate' utility to list the Command Line Tools
@@ -41,7 +43,7 @@ if ! xcode-select -p &>/dev/null; then
       # touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
       # PROD=$(softwareupdate -l | grep "\*.*Command Line" | tail -n 1 | sed 's/^[^C]* //')
       # softwareupdate -i "$PROD" --install-rosetta --agree-to-license --verbose
-      xcode-select --install && softwareupdate --install-rosetta --agree-to-license
+      xcode-select --install && sudo softwareupdate --install-rosetta --agree-to-license
     )
 fi
 
