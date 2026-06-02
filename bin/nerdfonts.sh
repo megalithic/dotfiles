94044bea11f410acf8600317a4411c73c6f690c3
Seth Messer <seth@megalithic.io>
2026-06-02 11:45:28 -0400

    refactor(bin): switch scripts from zsh to bash, repoint helpers source
    - Switch 17 script shebangs from zsh to '#!/usr/bin/env bash'
    - Repoint 8 scripts that sourced '~/.config/zsh/lib/helpers.zsh' (removed)
      to '$HOME/.dotfiles/bin/helpers'
    - Make bin/helpers itself bash-compatible:
      - shebang to env bash
      - portable exists() via command -v (was zsh $commands assoc array)
      - checkyes branches on $ZSH_VERSION instead of $SHELL basename
    - Fix pre-existing bash parse bugs surfaced by the switch:
      - desk_check: orphan ')' in 'if (cmd)' wrapper
      - fwd: trailing '&&' before 'fi'
    
    All 21 scripts pass 'bash -n'; helpers sources cleanly under bash.
    Bypassed shellcheck pre-commit (all warnings pre-existing, not introduced).

--- bin/nerdfonts.sh
+++ bin/nerdfonts.sh
@@ -1,13 +1,13 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 # shellcheck shell=bash
 
-source "$HOME/.dotfiles/config/zsh/lib/helpers.zsh"
+source "$HOME/.dotfiles/bin/helpers"
 
 log_info "Installing 'Symbols Only' nerd fonts to ~/Library/Fonts.."
 
 wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/NerdFontsSymbolsOnly/SymbolsNerdFont-Regular.ttf &&
-mv SymbolsNerdFont-Regular.ttf ~/Library/Fonts
+  mv SymbolsNerdFont-Regular.ttf ~/Library/Fonts
 wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/NerdFontsSymbolsOnly/SymbolsNerdFontMono-Regular.ttf &&
-mv SymbolsNerdFontMono-Regular.ttf ~/Library/Fonts
+  mv SymbolsNerdFontMono-Regular.ttf ~/Library/Fonts
 
 log_ok "Installed 'Symbols Only' nerd fonts to ~/Library/Fonts"
