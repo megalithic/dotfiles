3a0a6242f48666d20487fa27e05b4e6b33e23e7a
Seth Messer <seth@megalithic.io>
2026-07-28 09:49:04 -0400

    style(fish): compact starship init guard

--- mise/config/fish/conf.d/starship.fish
+++ mise/config/fish/conf.d/starship.fish
@@ -1,7 +1,1 @@
-# Starship prompt (non-nix twin of home/common/programs/starship, which used
-# programs.starship.enable to inject this into the HM-generated fish config).
-# Config lives at ~/.config/starship.toml (linked by the mise [dotfiles] table).
-status is-interactive; or exit
-command -sq starship; or exit
-
-starship init fish | source
+command -sq starship; and starship init fish | source
