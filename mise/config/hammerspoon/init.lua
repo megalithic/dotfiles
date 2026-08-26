2941f13d967012c8e1360ccc97f853351934bf87
Seth Messer <seth@megalithic.io>
2026-08-26 07:49:54 -0400

    feat(hammerspoon): enable pasteboard watcher, refresh EmmyLua stamps

--- mise/config/hammerspoon/init.lua
+++ mise/config/hammerspoon/init.lua
@@ -69,7 +69,7 @@ end
 
 hs.loadSpoon("EmmyLua")
 
-local watchers = { "audio", "notification", "url" }
+local watchers = { "audio", "notification", "url", "pasteboard" }
 -- local watchers = { "audio", "dock", "app", "notification", "url", "pasteboard", "screen", "media-presence" }
 
 req("bindings")
